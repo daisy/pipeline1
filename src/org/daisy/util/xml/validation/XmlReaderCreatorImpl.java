e39372b987b48fcf50097702826ac13cfeb2b131
Markus Gylling <markus.gylling@gmail.com>
2005-06-14 21:29:20 +0000

    minor cleanup fixes

--- src/org/daisy/util/xml/validation/XmlReaderCreatorImpl.java
+++ src/org/daisy/util/xml/validation/XmlReaderCreatorImpl.java
@@ -9,19 +9,16 @@ import org.xml.sax.SAXNotRecognizedException;
 import org.xml.sax.SAXNotSupportedException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.XMLReaderFactory;
-
 import com.thaiopensource.xml.sax.XMLReaderCreator;
 
 /**
+ * Custom implementation of Clarks XMLReaderCreator, supporting the set of {@link org.daisy.util.xml.dtdcatalog.CatalogEntityResolver}
  * @author markusg
  */
 
 public class XmlReaderCreatorImpl implements XMLReaderCreator{
-	//static SAXParserFactory factory = SAXParserFactory.newInstance();
 	
-	public XmlReaderCreatorImpl() {
-		
-	}
+	public XmlReaderCreatorImpl() {}
 	
 	public XMLReader createXMLReader() throws SAXException {		
 	    XMLReader xr;	    
@@ -33,28 +30,12 @@ public class XmlReaderCreatorImpl implements XMLReaderCreator{
 	      xr.setFeature("http://xml.org/sax/features/validation", false);
 	    }
 	    catch (SAXNotRecognizedException e) {
+	    	System.err.println("caught SAXNotRecognizedException in  org.daisy.util.validation.XmlReaderCreatorImpl");
 	    }
 	    catch (SAXNotSupportedException e) {
+	    	System.err.println("caught SAXNotSupportedException in  org.daisy.util.validation.XmlReaderCreatorImpl");
 	    }
-	    return xr;
-
-		
-		//		XMLReader xr = null;
-//		try {
-//			xr = factory.newSAXParser().getXMLReader();			
-//			xr.setEntityResolver(CatalogEntityResolver.getInstance());
-//			xr.setFeature("http://xml.org/sax/features/namespaces", true);
-//			xr.setFeature("http://xml.org/sax/features/namespace-prefixes", false);						
-//			xr.setFeature("http://xml.org/sax/features/validation", false);						
-//		} catch (SAXException se) {
-//			System.err.println("SAXException se");
-//		} catch (ParserConfigurationException pce) {
-//			System.err.println("ParserConfigurationException pce");
-//		}
-//		System.err.println(xr.getClass().getName());
-//		return xr;		
+	    
+	    return xr;		
 	}
 }
-
-
-
