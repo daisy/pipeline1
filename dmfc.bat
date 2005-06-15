4be80add988211f886f25b439a33998f23e28b7d
Linus Ericson <linus.ericson@tpb.se>
2005-06-15 12:00:16 +0000

    updated with latest classpath

--- dmfc.bat
+++ dmfc.bat
@@ -1,4 +1,4 @@
 @echo off
 echo.
-java -classpath "%~p0bin";"%~p0lib/jing.jar";"%~p0lib/saxon.jar";"%~p0lib/saxon8.jar" org.daisy.dmfc.ui.CommandLineUI %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath "%~p0bin";"%~p0lib/jing.jar";"%~p0lib/saxon.jar";"%~p0lib/saxon8.jar";"%~p0lib/sac.jar";"%~p0lib/batik-css.jar";"%~p0lib/batik-util.jar" org.daisy.dmfc.ui.CommandLineUI %1 %2 %3 %4 %5 %6 %7 %8 %9
 echo.
--- dmfc.sh
+++ dmfc.sh
@@ -2,6 +2,6 @@
 
 JAVA=java
 BASE=`dirname $0`
-CP="$BASE/bin":"$BASE/lib/jing.jar":"$BASE/lib/saxon.jar":"$BASE/lib/saxon8.jar"
+CP="$BASE/bin":"$BASE/lib/jing.jar":"$BASE/lib/saxon.jar":"$BASE/lib/saxon8.jar":"$BASE/lib/sac.jar":"$BASE/lib/batik-css.jar":"$BASE/lib/batik-util.jar"
 
 $JAVA -classpath $CP org.daisy.dmfc.ui.CommandLineUI $@
