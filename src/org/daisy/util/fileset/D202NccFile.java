712fd82fdc5e891f3c021fdca9d1d7dc320baa6e
Markus Gylling <markus.gylling@gmail.com>
2005-06-21 13:55:18 +0000

    fetching stated totaltime

--- src/org/daisy/util/fileset/D202NccFile.java
+++ src/org/daisy/util/fileset/D202NccFile.java
@@ -5,5 +5,5 @@ package org.daisy.util.fileset;
  * @author Markus Gylling
  */
 public interface D202NccFile extends Xhtml10File, ManifestFile{
-
+	public SmilClock getStatedDuration();
 }
--- src/org/daisy/util/fileset/D202NccFileImpl.java
+++ src/org/daisy/util/fileset/D202NccFileImpl.java
@@ -7,12 +7,14 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.Attributes;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 /**
  * Represents the Navigation Control Center (NCC) file in a Daisy 2.02 fileset
  * @author Markus Gylling
  */
 class D202NccFileImpl extends Xhtml10FileImpl implements D202NccFile {
+	private SmilClock myStatedDuration = null;
 	
     D202NccFileImpl(URI uri) throws ParserConfigurationException, SAXException, IOException {
         super(uri);          
@@ -30,7 +32,23 @@ class D202NccFileImpl extends Xhtml10FileImpl implements D202NccFile {
 				putIdValue(attrValue);
 			}else if (regex.matches(regex.XHTML_ATTRS_WITH_URIS,attrName)) {
 				putUriValue(attrValue);
-			}					
+			}
+			
+			try {
+				if (sName=="meta") {
+					if (attrValue=="ncc:totalTime") {
+						myStatedDuration = new SmilClock(attrs.getValue("content"));
+					}
+				}				
+			} catch (Exception nfe) {
+				this.listeningErrorHandler.error(new SAXParseException(this.getName()+": exception when calculating " +attrValue,null));
+				
+			}
+			
 		} //for (int i
-	}			
+	}	
+	
+	public SmilClock getStatedDuration() {				
+		return myStatedDuration;
+	}
 }
