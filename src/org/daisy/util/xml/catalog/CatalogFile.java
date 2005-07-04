8fdd861ce90b720e1319c280a7c0bac762460c10
Markus Gylling <markus.gylling@gmail.com>
2005-07-04 13:36:28 +0000

    removed unused imports

--- src/org/daisy/util/xml/catalog/CatalogFile.java
+++ src/org/daisy/util/xml/catalog/CatalogFile.java
@@ -5,7 +5,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.Hashtable;
