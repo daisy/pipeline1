f1d2b724cae9019d3a6d729b04ac2e205814d201
Linus Ericson <linus.ericson@tpb.se>
2005-09-29 08:34:43 +0000

    added getter methods

--- src/org/daisy/dmfc/core/MIMEType.java
+++ src/org/daisy/dmfc/core/MIMEType.java
@@ -75,4 +75,10 @@ public class MIMEType {
     public String toString() {
         return name;
     }
+    public String getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
 }
