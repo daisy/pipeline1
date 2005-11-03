e45a4761a8e17d61c2e477f45e5fc5dbd19280e5
Linus Ericson <linus.ericson@tpb.se>
2005-11-03 14:23:37 +0000

    updated classpath

--- dmfc.bat
+++ dmfc.bat
@@ -1,4 +1,4 @@
 @echo off
 echo.
-java -classpath "%~p0bin";"%~p0lib/jing.jar";"%~p0lib/saxon.jar";"%~p0lib/saxon8.jar";"%~p0lib/sac.jar";"%~p0lib/batik-css.jar";"%~p0lib/batik-util.jar" org.daisy.dmfc.ui.CommandLineUI %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath "%~p0bin";"%~p0lib/jing.jar";"%~p0lib/saxon.jar";"%~p0lib/saxon8.jar";"%~p0lib/sac.jar";"%~p0lib/batik-css.jar";"%~p0lib/batik-util.jar";"%~p0lib/jsr173_1.0_api.jar";"%~p0lib/wstx-lgpl-2.0.3.jar";"%~p0lib/jl1.0.jar" org.daisy.dmfc.ui.CommandLineUI %1 %2 %3 %4 %5 %6 %7 %8 %9
 echo.
