762ba50e1f280c0e7cec3c77d3df292c5ad7f1db
Linus Ericson <linus.ericson@tpb.se>
2005-12-22 12:46:53 +0000

    initial commit

---
+++ transformers/se_tpb_mixedContentNormalizer/AttributedContext.java
@@ -0,0 +1,78 @@
+/*
+ * DMFC - The DAISY Multi Format Converter
+ * Copyright (C) 2005  Daisy Consortium
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package se_tpb_mixedContentNormalizer;
+
+import java.util.Iterator;
+import java.util.Stack;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.events.XMLEvent;
+
+/*package*/ class AttributedContext {
+    
+    protected class ContextInfo {
+        public ContextInfo(QName n) {
+            name = n;
+        }
+        public QName name;
+        public boolean hasElements = false;
+        public boolean hasText = false;
+        public boolean spanOpen = false;
+        public int number = 0;
+    }
+    
+    protected Stack context = new Stack();
+      
+    public boolean register(XMLEvent event) {
+        if (event.isStartElement()) {            
+            context.push(new ContextInfo(event.asStartElement().getName()));
+        } else if (event.isEndElement()) {
+            context.pop();
+        }
+        return true;
+    }
+    
+    public ContextInfo peek() {
+        return (ContextInfo)context.peek();
+    }
+    
+    public boolean isEmpty() {
+        return context.empty();
+    }
+    
+    public boolean hasParent() {
+        return context.size() > 1;
+    }
+    
+    public ContextInfo getParent() {
+        return (ContextInfo)context.elementAt(context.size() - 2);
+    }
+    
+    public String getContextPath() {
+        StringBuffer buffer = new StringBuffer();
+        for (Iterator it = context.iterator(); it.hasNext(); ) {
+            QName name = ((ContextInfo)it.next()).name;
+            buffer.append("/").append(name.getLocalPart());
+        }
+        if (context.isEmpty()) {
+            buffer.append("/");
+        }
+        return buffer.toString();
+    }    
+}
---
+++ transformers/se_tpb_mixedContentNormalizer/EventWriterCache.java
@@ -0,0 +1,77 @@
+/*
+ * DMFC - The DAISY Multi Format Converter
+ * Copyright (C) 2005  Daisy Consortium
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package se_tpb_mixedContentNormalizer;
+
+import javax.xml.stream.XMLEventWriter;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.XMLEvent;
+
+/**
+ * Base class and factory for caching event writers.
+ * @author Linus Ericson
+ */
+abstract class EventWriterCache {
+
+    public static final int STRICT = 0;
+    public static final int RELAXED = 1;
+    
+    protected XMLEventWriter xew = null;
+    
+    protected EventWriterCache(XMLEventWriter writer) {
+        xew = writer;
+    }
+    
+    public static EventWriterCache newInstance(XMLEventWriter writer, int strictness) {
+        if (strictness == STRICT) {
+            return new SingleEventCache(writer);
+        }
+        return new DoubleEventCache(writer);
+    }
+    
+    public abstract void writeEvent(XMLEvent ev, boolean isSpan) throws XMLStreamException;
+    public abstract void flush() throws XMLStreamException;
+        
+    protected String tagInfo(XMLEvent ev) {
+        StringBuffer buffer = new StringBuffer();
+        switch (ev.getEventType()) {
+        case XMLStreamConstants.START_ELEMENT:
+            buffer.append("StartElement: ");
+        	buffer.append(ev.asStartElement().getName().getLocalPart());        	
+            break;
+        case XMLStreamConstants.END_ELEMENT:
+            buffer.append("EndElement: ");
+        	buffer.append(ev.asEndElement().getName().getLocalPart());
+            break;
+        case XMLStreamConstants.CHARACTERS:
+            buffer.append("Characters: ");
+        	buffer.append(ev.asCharacters().getData());
+            break;
+        case XMLStreamConstants.START_DOCUMENT:
+            buffer.append("StartDocument. ");
+            break;
+        case XMLStreamConstants.END_DOCUMENT:
+            buffer.append("EndDocument. ");
+            break;       
+        default:
+            break;
+        }
+        return buffer.toString();
+    }
+}
