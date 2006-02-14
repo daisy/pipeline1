868ee7b79dafc7dc4d602e6682d72d8d4f2f7019
Linus Ericson <linus.ericson@tpb.se>
2006-02-14 14:28:45 +0000

    initial commit

---
+++ src/org/daisy/dmfc/core/ProgressObserver.java
@@ -0,0 +1,35 @@
+/*
+ * DMFC - The DAISY Multi Format Converter
+ * Copyright (C) 2006  Daisy Consortium
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.daisy.dmfc.core;
+
+/**
+ * Observer of progress.
+ * This interface is typically implemented by subclasses of the
+ * <code>org.daisy.dmfc.core.transformer.Transformer</code> class and used by
+ * other classes to report progress back to the transformer.
+ * @author Linus Ericson
+ */
+public interface ProgressObserver {
+    
+    /**
+     * Report a progress event.
+     * @param progress the current progress in the interval [0,1]
+     */
+    public void reportProgress(double progress);
+}
