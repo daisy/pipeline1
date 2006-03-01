a16735562665746646486227498021de18b950f2
Laurie Sherve <lauriesherve@users.sourceforge.net>
2006-03-01 23:36:44 +0000

    Updated execute() method

--- src/org/daisy/dmfc/qmanager/QueueRunner.java
+++ src/org/daisy/dmfc/qmanager/QueueRunner.java
@@ -2,15 +2,23 @@ package org.daisy.dmfc.qmanager;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
 
+import javax.naming.ConfigurationException;
+
+import org.daisy.dmfc.core.DMFCCore;
+import org.daisy.dmfc.core.EventListener;
+import org.daisy.dmfc.core.InputListener;
 import org.daisy.dmfc.core.script.ScriptHandler;
+import org.daisy.dmfc.exception.DMFCConfigurationException;
 import org.daisy.dmfc.exception.ScriptException;
 
 
 
 
 /**
- * For each job in the Queue, calls the execute method in the ScriptHandler
+ * For each job (script)in the Queue, calls the execute method in the DMFCCore
  * @author Laurie Sherve
  *
  */
@@ -27,14 +35,20 @@ public class QueueRunner  {
 	/**
 	 * To run the Queue:
 	 * Each Queue has many jobs
-	 * Each job has a script
+	 * Each job is/has a script
 	 * Each script has one-to-many tasks 
-	 * Each task is executed by the ScriptHandler
-	 *
+	 * Each script is executed by the DMFCCore 
+	 * (and each task is executed by the ScriptHandler)
+	 * 
+	 *@param il InputListener interface - for concrete web or local IL
+	 *@param el EventListener interface - for concrete web or local EL
 	 *@throws ScriptException on execute()
 	 */
-	public void executeJobsInQueue()throws ScriptException{
+	public void executeJobsInQueue(InputListener il, EventListener el){
 		
+		/** @todo possibly put this in a properties file? Does this differ from script to script?*/
+		Locale locale = new Locale("en", "US");
+			
 		//walk through the queue and return jobs
 		LinkedList jobList = queue.getLinkedListJobs();
 		Iterator it = jobList.iterator();
@@ -46,16 +60,19 @@ public class QueueRunner  {
 			//get the task script
 			ScriptHandler script = job.getScript();
 			
-			//fill in the parameters in each job script
-			/**
-			 * @todo how to determine the "name" property for input and output?
-			 * The value should be correct
-			 */
-			script.setProperty(script.getName(), job.getInputFile().getName());
-			script.setProperty(script.getName(), job.getOutputFile().getName());
-			
-			//execute the script
-			script.execute();
+			try{
+				DMFCCore dmfc = new DMFCCore(il, el);
+				script.setProperty("input", job.getInputFile().getName());
+				script.setProperty("outputDir", job.getOutputFile().getName());
+				dmfc.executeScript(script);
+			}
+			catch(ScriptException se){
+				//add error messages to be thrown to GUI
+			}
+			catch (DMFCConfigurationException e) {  
+				//add error messages to be thrown to GUI
+	               e.printStackTrace();
+			}
 			
 		}
 	}
