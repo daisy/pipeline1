9cf25880a061bcd4f9c0a630bfb556e29c8fe9b4
Laurie Sherve <lauriesherve@users.sourceforge.net>
2006-03-02 00:15:49 +0000

    Added Timing

--- src/org/daisy/dmfc/qmanager/LocalEventListener.java
+++ src/org/daisy/dmfc/qmanager/LocalEventListener.java
@@ -1,9 +1,11 @@
 package org.daisy.dmfc.qmanager;
 
+import java.util.HashMap;
 import java.util.logging.Level;
 
 import org.daisy.dmfc.core.EventListener;
 import org.daisy.dmfc.core.Prompt;
+import org.daisy.util.fileset.SmilClock;
 
 public class LocalEventListener implements EventListener{
 
@@ -17,6 +19,15 @@ public class LocalEventListener implements EventListener{
 	    if (prompt.getType() == Prompt.TRANSFORMER_END) {
 	        System.out.println("Transformer " + prompt.getMessageOriginator() + " has just finished running");
 	    }
+	    if (prompt.getType() ==Prompt.PROGRESS){
+	    	calculateTiming(prompt);
+	    }
+	}
+	public HashMap calculateTiming(Prompt prompt){
+		HashMap hm = new HashMap();
+		hm.put("left", prompt.getTimeLeft());
+		hm.put("progress", prompt.getTotalTime());
+		return hm;	
 	}
 
 	
