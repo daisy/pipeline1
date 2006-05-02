5af73187676e58bd437ea8d5a84edd13632e5652
Stefan Hofstetter <shofstet@users.sourceforge.net>
2006-05-02 14:36:23 +0000

    automatic including all jar library in ./lib/*.jar

--- dmfc.sh
+++ dmfc.sh
@@ -5,8 +5,16 @@ BASE=`dirname $0`
 
 BIN=$BASE/bin
 LIB=$BASE/lib
+    
 
 
-CP="$BIN":"$LIB/jing.jar":"$LIB/saxon.jar":"$LIB/saxon8.jar":"$LIB/sac.jar":"$LIB/batik-css.jar":"$LIB/batik-util.jar":"$LIB/jl1.0.jar":"$LIB/jsr173_1.0_api.jar":"$LIB/wstx-lgpl-2.0.3.jar":"$LIB/saxon8-dom.jar"
+LIB_TMP=
+for jar in `ls $LIB/*.jar`; do
+    LIB_TMP="$jar":$LIB_TMP
+done
+
+CP="$BIN":"$LIB_TMP"
+
+#echo $CP
 
 $JAVA -classpath $CP org.daisy.dmfc.ui.CommandLineUI $@
