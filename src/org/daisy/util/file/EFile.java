7677b17373389be7a9231be33965931fcaa595ec
Markus Gylling <markus.gylling@gmail.com>
2006-05-04 22:09:36 +0000

    now a class, decoupled from the interface (see IEFile)

--- src/org/daisy/util/file/EFile.java
+++ src/org/daisy/util/file/EFile.java
@@ -1,87 +1,120 @@
 package org.daisy.util.file;
 
 import java.io.File;
-import java.io.FileFilter;
-import java.io.FilenameFilter;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URI;
-import java.net.URL;
 
 import org.daisy.util.mime.MIMEType;
 import org.daisy.util.mime.MIMETypeException;
-import org.daisy.util.mime.MIMETypeFactory;
 import org.daisy.util.mime.MIMETypeFactoryException;
+import org.daisy.util.mime.MIMETypeFactory;
 
 /**
- *   
- * @author Markus Gylling 
+ * EFile - where E stands for extended.
+ * @author Markus Gylling
  */
-public abstract interface EFile {
+public class EFile extends java.io.File  {
+
+	private static MIMETypeFactory mimeFactory = MIMETypeFactory.newInstance();
+	
+	private MIMEType mimeType = null;
+	
+	public EFile(String pathname) {
+		super(pathname);
+	}
+
+	public EFile(String parent, String child) {
+		super(parent, child);
+	}
+
+	public EFile(java.io.File parent, String child) {
+		super(parent, child);
+	}
+
+	public EFile(EFolder parent, String child) {
+		super(parent, child);
+	}
+	
+	public EFile(URI uri) {
+		super(uri);
+	}
+
+	public EFile(URI uri, String mimeString) throws MIMETypeException {
+		super(uri);
+		this.setMimeType(mimeString);
+	}
 			
-	/**
-	 * An extension to java.io.File.
-	 */
-	public boolean isSymLink() throws IOException ;
-
-	/**
-	 * An extension to java.io.File.
-	 * @return the MimeType if it is set, else null.
-	 */
-	public MIMEType getMimeType();
-
-	/**
-	 * An extension to java.io.File.
-	 * Attempts to retrieve a org.daisy.util.mime.MimeType object
-	 * based on the inparam string.
-	 * Note - no analysis of the relational validity of the MimeType object vis-a-vis the File is performed.
-	 * @param 
-	 * 		mime A MIME string identifier that may or may not be
-	 * represented in the local MIME registry (org.daisy.util.mime.MimeTypeRegistry).
-	 * @return 
-	 * 		the MimeType object if set succeeded, else a MimeTypeException
-	 * @see #getMimeType()
-	 */
-	public MIMEType setMimeType(String mime) throws MIMETypeException;
+	public EFile(File file) {
+		super(file.toURI());
+	}
 	
-	/**
-	 * An extension to java.io.File.
-	 * Attempts to retrieve a org.daisy.util.mime.MimeType object
-	 * by heuristical analysis of properties of this File.
-	 * @return 
-	 * 		the MimeType object if set succeeded, else a MimeTypeException
-	 */
-	public MIMEType setMimeType() throws MIMETypeException;
-	
-	/**
-	 * An extension to java.io.File.
-	 * Sets a org.daisy.util.mime.MimeType object as an attribute of this File.
-	 * Note - no analysis of the relational validity of the MimeType object vis-a-vis the File is performed. 
-	 * @see #setMimeType()
-	 * @see #getMimeType()
-	 */
-	public void setMimeType(MIMEType mime);
-	
-	/**
-	 * An extension to java.io.File.
-	 */
-	public EFolder getParentFolder() throws IOException ;
-	
-	/**
-	 * An extension to java.io.File.
-	 * @return the name of the File
-	 * minus extension; ("foo" from "foo.bar").
-	 * If the name has no extension (no period character)
-	 * then this method returns the same String as File.getName().
-	 */
-	public String getNameMinusExtension();
+	public boolean isSymLink() throws IOException {
+		return this.getCanonicalFile().equals
+				(this.getAbsoluteFile());
+	}
 
-	/**
-	 * An extension to java.io.File.
-	 * @return the extension of the File; ("bar" from "foo.bar").
-	 * If the name has no extension (no period character)
-	 * then this method returns null.
-	 */
-	public String getExtension();
+	public MIMEType getMimeType() {
+		return this.mimeType;
+	}
 
+	public MIMEType setMimeType(String mime) throws MIMETypeException {
+		try{
+		  return this.mimeType = mimeFactory.newMimeType(mime); 		  
+		} catch  (MIMETypeFactoryException mfe) {
+			throw new MIMETypeException(mfe.getMessage(),mfe);
+		}				
+	}
+	
+	public MIMEType setMimeType() throws MIMETypeException {
+		try{
+			return this.mimeType = mimeFactory.newMimeType(this);
+		} catch (Exception e) {
+			throw new MIMETypeException(e.getMessage(),e);
+		}
+	}
+	
+	public void setMimeType(MIMEType mime) {
+		  this.mimeType = mime; 
+	}
+	
+	public EFolder getParentFolder() throws IOException{
+		return new EFolder(this.getParent());
+	}
+	
+	public String getName(){
+		return super.getName();
+	}
+	
+	public String getNameMinusExtension() {
+		StringBuilder sb = new StringBuilder();
+		String name = this.getName();
+		int end = name.lastIndexOf('.');
+		if(end > 0) {
+			for (int i = 0; i < name.length(); i++) {
+				if(i<end){
+					sb.append(name.charAt(i));
+				}
+			}
+			return sb.toString();
+		}
+		return name;
+	}
+
+	public String getExtension() {
+		StringBuilder sb = new StringBuilder();
+		String name = this.getName();
+		int start = name.lastIndexOf('.');
+		if(start > 0) {
+			for (int i = 0; i < name.length(); i++) {
+				if(i>start){
+					sb.append(name.charAt(i));
+				}
+			}
+			return sb.toString();
+		}
+		return null;
+	}
+
+	private static final long serialVersionUID = 11152264979926847L;
+	
 }
