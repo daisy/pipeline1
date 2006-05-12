1506b5c43eeb0bc5c1f591a53b807e88d5c48c50
lauriesherve <lauriesherve@27897e58-4529-0410-8da3-928601d0d17f>
2006-05-12 18:10:43 +0000

    changed file selection for only compatible files

--- dmfcGUI/src/org/daisy/dmfc/gui/widgetproperties/CompatibleFilesTableProperties.java
+++ dmfcGUI/src/org/daisy/dmfc/gui/widgetproperties/CompatibleFilesTableProperties.java
@@ -1,7 +1,13 @@
 package org.daisy.dmfc.gui.widgetproperties;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
+import org.daisy.util.file.EFile;
+import org.daisy.util.file.EFolder;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
@@ -30,21 +36,55 @@ public class CompatibleFilesTableProperties {
 		
 	}
 	
-	public void populateTable(File file){
-		if (file.isDirectory()){
-			File [] arFiles = file.listFiles();
-			
-			for (int i = 0; i<arFiles.length; i++){
-				
-				File subFiles = arFiles[i];
-				
-				//only show  files
-				if (subFiles.isFile()){
-					TableItem ti = new TableItem(table ,SWT.NONE,0);
-					ti.setText(subFiles.getAbsolutePath());
-				}
-			}
+	public void populateTable(File file, ArrayList alPatterns){
+		
+		ArrayList alCompatibleFiles = new ArrayList();
+		
+		EFolder eFolder=null;
+		try {
+			 eFolder = new EFolder(file.getPath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
+		Collection recursiveFolders = eFolder.getFolders(true, ".+", false);
+		Iterator itFolders = recursiveFolders.iterator();
+		
+		while (itFolders.hasNext()){
+			File folderFile = (File)itFolders.next();
+			System.out.println("Names of all folders : " + folderFile.getPath());
+			
+			//get all files in this folder
+			File [] arFiles = folderFile.listFiles();
+		
+			for (int j=0; j<arFiles.length; j++){
+				//for each file, create an EFile
+				File compareFile = arFiles[j];
+				EFile eFile = new EFile(compareFile.getPath());
+				String strExtension = eFile.getExtension();
+				
+//				compare them to the mimetype pattern(s) and 
+				//only place compatible types in the array
+				
+				Iterator itMimeTypes = alPatterns.iterator();
+				while (itMimeTypes.hasNext()){
+					if (strExtension.equalsIgnoreCase((String)itMimeTypes.next()))
+						alCompatibleFiles.add(compareFile);
+					
+				}
+					
+			}
+			
+		}
+		
+		
+		
+		Iterator itCompatible = alCompatibleFiles.iterator();
+		while (itCompatible.hasNext()){
+			TableItem ti = new TableItem(table ,SWT.NONE,0);
+			ti.setText((String)itCompatible.next());
+		}	
 	}
 	
 }
