1304c2292c5883c9d522e2edf49a3ee554c39233
Markus Gylling <markus.gylling@gmail.com>
2006-05-14 13:23:08 +0000

    improved javadoc

--- src/org/daisy/util/xml/Peeker.java
+++ src/org/daisy/util/xml/Peeker.java
@@ -15,21 +15,43 @@ import org.xml.sax.SAXException;
 public interface Peeker {
 	public void peek(URI uri) throws SAXException, IOException;
 	/**
-	 * @return the first encountered public id while parsing the file
+	 * @return the first encountered public id while parsing the file; that of the document entity
 	 */
 	public String getFirstPublicId();
 	/**
-	 * @return the first encountered system id while parsing the file
+	 * @return the first encountered system id while parsing the file; that of the document entity
 	 */
 	public String getFirstSystemId();
+	
+	/**
+	 * @return the local name of the documents root element
+	 */
 	public String getRootElementLocalName();
+
+	/**
+	 * @return the namespace URI of the root element
+	 */
 	public String getRootElementNsUri();
-	public QName getRootElementQName();	
+
+	/**
+	 * @return the root element represented as a QName object
+	 */
+	public QName getRootElementQName();
+	
+	/**
+	 * @return the XML version as stated in the XML declaration pseudoattribute
+	 */	
 	public String getXMLVersion();
 	/**
 	 * @return the encoding if stated in prolog, null if no encoding pseudoattr
 	 */
 	public String getEncoding();
+	
+	/**
+	 * @return the standalone value as stated in the XML declaration pseudoattribute
+	 */	
 	public boolean getStandalone();
+	
+	
 	public void reset();
 }
