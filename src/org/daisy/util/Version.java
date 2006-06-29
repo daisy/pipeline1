dc484496aa8c154e7b3ecb7bf83c1e8e2ced56d7
Markus Gylling <markus.gylling@gmail.com>
2006-06-29 18:45:53 +0000

    first commit

---
+++ src/org/daisy/util/Version.java
@@ -0,0 +1,15 @@
+package org.daisy.util;
+
+/**
+ * Exposes global version information on the whole org.daisy.util package
+ * @author Markus Gylling
+ */
+public class Version {
+
+	private static final String VERSION = "1.0.0";
+	
+	public static String getVersion() {
+		return VERSION;
+	}
+	
+}
