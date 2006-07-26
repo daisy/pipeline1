0563ae33c86327b68bec08e184fcce7c3a8745bf
Piotr Kiernicki <piotr.kiernicki@mtm.se>
2006-07-26 14:07:01 +0000

    reverted to previous version

--- src/org/daisy/util/xml/Peeker.java
+++ src/org/daisy/util/xml/Peeker.java
@@ -2,11 +2,9 @@ package org.daisy.util.xml;
 
 import java.io.IOException;
 import java.net.URI;
-import java.net.URL;
 
 import javax.xml.namespace.QName;
 
-import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 /**
  * Peeks into an XML file, returns 
@@ -15,53 +13,45 @@ import org.xml.sax.SAXException;
  */
 
 public interface Peeker {
-	public void peek(URI uri) throws SAXException, IOException;
-	
-	public void peek(URL url) throws SAXException, IOException;
+    public void peek(URI uri) throws SAXException, IOException;
+    /**
+     * @return the first encountered public id while parsing the file; that of the document entity
+     */
+    public String getFirstPublicId();
+    /**
+     * @return the first encountered system id while parsing the file; that of the document entity
+     */
+    public String getFirstSystemId();
+    
+    /**
+     * @return the local name of the documents root element
+     */
+    public String getRootElementLocalName();
 
-	/**
-	 * @return the first encountered public id while parsing the file; that of the document entity
-	 */
-	public String getFirstPublicId();
-	/**
-	 * @return the first encountered system id while parsing the file; that of the document entity
-	 */
-	public String getFirstSystemId();
-	
-	/**
-	 * @return the local name of the documents root element
-	 */
-	public String getRootElementLocalName();
+    /**
+     * @return the namespace URI of the root element
+     */
+    public String getRootElementNsUri();
 
-	/**
-	 * @return the attributes of the root element
-	 */
-	public Attributes getRootElementAttributes();
-	
-	/**
-	 * @return the namespace URI of the root element
-	 */
-	public String getRootElementNsUri();
-
-	/**
-	 * @return the root element represented as a QName object
-	 */
-	public QName getRootElementQName();
-	
-	/**
-	 * @return the XML version as stated in the XML declaration pseudoattribute
-	 */	
-	public String getXMLVersion();
-	/**
-	 * @return the encoding if stated in prolog, null if no encoding pseudoattr
-	 */
-	public String getEncoding();
-	
-	/**
-	 * @return the standalone value as stated in the XML declaration pseudoattribute
-	 */	
-	public boolean getStandalone();
-	
-	
-	public void reset();
+    /**
+     * @return the root element represented as a QName object
+     */
+    public QName getRootElementQName();
+    
+    /**
+     * @return the XML version as stated in the XML declaration pseudoattribute
+     */ 
+    public String getXMLVersion();
+    /**
+     * @return the encoding if stated in prolog, null if no encoding pseudoattr
+     */
+    public String getEncoding();
+    
+    /**
+     * @return the standalone value as stated in the XML declaration pseudoattribute
+     */ 
+    public boolean getStandalone();
+    
+    
+    public void reset();
 }
--- src/org/daisy/util/xml/PeekerImpl.java
+++ src/org/daisy/util/xml/PeekerImpl.java
@@ -1,26 +1,21 @@
 package org.daisy.util.xml;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URI;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.xml.namespace.QName;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
-import javax.xml.transform.sax.SAXSource;
-import javax.xml.transform.stream.StreamSource;
 
 import org.daisy.util.exception.SAXStopParsingException;
 import org.daisy.util.xml.catalog.CatalogEntityResolver;
-import org.daisy.util.xml.pool.PoolException;
-import org.daisy.util.xml.pool.SAXParserPool;
-import org.daisy.util.xml.pool.StAXInputFactoryPool;
-import org.daisy.util.xml.sax.SAXConstants;
 import org.daisy.util.xml.stax.StaxEntityResolver;
 import org.xml.sax.Attributes;
 import org.xml.sax.ContentHandler;
@@ -37,244 +32,302 @@ import org.xml.sax.ext.LexicalHandler;
  * @author Markus Gylling
  */
 public class PeekerImpl
-		implements
-			ContentHandler,
-			ErrorHandler,
-			DTDHandler,
-			LexicalHandler,
-			EntityResolver,
-			Peeker {
-	//private static SAXParserFactory factory;
-	private SAXParser parser;
-	private static Map saxParserFeatures;
-	private static Map staxParserProperties;
-	//private static XMLInputFactory xif = null;
-	private XMLStreamReader xer = null;
-	//private URI currentURI = null;
-	private URL currentURL = null;
+        implements
+            ContentHandler,
+            ErrorHandler,
+            DTDHandler,
+            LexicalHandler,
+            EntityResolver,
+            Peeker {
+    private static SAXParserFactory factory;
+    private static SAXParser parser;
+    private static XMLInputFactory xif = null;
+    private XMLStreamReader xer = null;
+    private URI currentURI = null;
 
-	private String rootElementNsUri = "";
-	private String rootElementLocalName = "";
-	private String firstPublicId = "";
-	private String firstSystemId = "";
+    private String rootElementNsUri ="";
+    private String rootElementLocalName = "";
+    private String firstPublicId = "";
+    private String firstSystemId = "";
 
-	private String xmlEncoding = "";
-	private String xmlVersion = "";
-	private String xmlStandalone = "";
-	private Attributes rootElementAttributes = null;
+    private String xmlEncoding = "";
+    private String xmlVersion = "";
+    private String xmlStandalone = "";
 
-	public PeekerImpl() {
+    public PeekerImpl() {
 
-		if(saxParserFeatures == null){
-			saxParserFeatures = new HashMap();
-			saxParserFeatures.put(SAXConstants.SAX_FEATURE_NAMESPACES, Boolean.TRUE);
-			saxParserFeatures.put(SAXConstants.SAX_FEATURE_VALIDATION, Boolean.FALSE);     
-		}
-		    	    	
-		try {
-			parser = SAXParserPool.getInstance().acquire(saxParserFeatures,null);   	
-	    	parser.getXMLReader().setErrorHandler(this);
-	    	parser.getXMLReader().setContentHandler(this);
-	    	parser.getXMLReader().setDTDHandler(this);
-	    	parser.getXMLReader().setEntityResolver(CatalogEntityResolver.getInstance());
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
-	}
+        if (factory == null) {
+            factory = SAXParserFactory.newInstance();
+            factory.setValidating(false);
+            factory.setNamespaceAware(true);
+        }
+        try {
+            parser = factory.newSAXParser();
+            parser.getXMLReader().setContentHandler(this);
+            parser.getXMLReader().setErrorHandler(this);
+            parser.getXMLReader().setDTDHandler(this);
+            parser.getXMLReader().setEntityResolver(this);
+        } catch (ParserConfigurationException e) {
+            System.err.println("ParserConfigurationException in peeker: "
+                    + e.getMessage());
+        } catch (SAXException e) {
+            System.err.println("SAXException in peeker: "
+                    + e.getMessage());
+        }
+    }
 
-	public void peek(URI uri) throws SAXException, IOException {
-		peek(uri.toURL());
-	}
+    public void peek(URI uri)
+            throws SAXException, IOException {
+        reset();
+        currentURI = uri;
+        if (parser != null) {
+            try {
+                File f = new File(uri);
+                parser.getXMLReader().parse(new InputSource(new FileInputStream(f)));
+                // we never get here since SAXStopParsingException is thrown
+            } catch (SAXStopParsingException sspe) {
+                //
+            }
+        } else {
+            throw new SAXException("peeker parser is null");
+        }
+    }
 
-	public void peek(URL url) throws SAXException, IOException {
-		reset();
-		currentURL = url;
-		if (parser != null) {
-			try {				
-				parser.getXMLReader().parse(SAXSource.sourceToInputSource(new StreamSource(url.openStream())));
-				// we never get here since SAXStopParsingException is thrown
-			} catch (SAXStopParsingException sspe) {
-				try {
-					SAXParserPool.getInstance().release(parser,saxParserFeatures,null);
-				} catch (PoolException e) {
-					throw new SAXException(e.getMessage(),e);
-				}
-			}
-		} else {
-			throw new SAXException("peeker parser is null");
-		}
-	}
-	
-	public void reset() {
-		xer = null;
-		//currentURI = null;
-		currentURL = null;
-		// make the getter vars the empty string to allow for less tests when
-		// retreiving value
-		rootElementNsUri = "";
-		rootElementLocalName = "";
-		firstPublicId = "";
-		firstSystemId = "";
-		xmlEncoding = "";
-		xmlVersion = "";
-		xmlStandalone = "";
-	}
+    public void reset() {
+        xer = null;
+        currentURI = null;
+        //make the getter vars the empty string to allow for less tests when retreiving value
+        rootElementNsUri = "";
+        rootElementLocalName = "";
+        firstPublicId = "";
+        firstSystemId = "";
+        xmlEncoding = "";
+        xmlVersion = "";
+        xmlStandalone = "";
+    }
 
-	public String getFirstPublicId() {
-		return firstPublicId;
-	}
+    public String getFirstPublicId() {
+        return firstPublicId;
+    }
 
-	public String getFirstSystemId() {
-		return firstSystemId;
-	}
+    public String getFirstSystemId() {
+        return firstSystemId;
+    }
 
-	public String getRootElementLocalName() {
-		return rootElementLocalName;
-	}
+    public String getRootElementLocalName() {
+        return rootElementLocalName;
+    }
 
-	public String getRootElementNsUri() {
-		return rootElementNsUri;
-	}
+    public String getRootElementNsUri() {
+        return rootElementNsUri;
+    }
 
-	public Attributes getRootElementAttributes() {
-		return this.rootElementAttributes;
-	}
-	
-	public QName getRootElementQName() {
-		QName q = null;
-		if (rootElementNsUri.length() > 0
-				&& rootElementLocalName.length() > 0) {
-			q = new QName(rootElementNsUri, rootElementLocalName);
-		}
-		return q;
-	}
+    public QName getRootElementQName() {
+        QName q = null;
+        if (rootElementNsUri.length() > 0
+                && rootElementLocalName.length() > 0) {
+            q = new QName(rootElementNsUri, rootElementLocalName);
+        }
+        return q;
+    }
 
-	public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
-		this.rootElementLocalName = localName;
-		this.rootElementNsUri = uri;
-		this.rootElementAttributes  = atts;
-		throw new SAXStopParsingException();
-	}
+    public void startElement(String uri,
+            String localName, String qName,
+            Attributes atts) throws SAXException {
+        this.rootElementLocalName = localName;
+        this.rootElementNsUri = uri;
+        throw new SAXStopParsingException();
+    }
 
-	public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
-		if (publicId != null) {
-			this.firstPublicId = publicId;
-		}
-		if (systemId != null) {
-			this.firstSystemId = systemId;
-		}
+    public void endElement(String uri,
+            String localName, String qName)
+            throws SAXException {
+    }
 
-		return CatalogEntityResolver.getInstance().resolveEntity(publicId, systemId);
-	}
+    public void setDocumentLocator(Locator locator) {
+    }
 
-	// ********************************************************
-	// below methods that force reparse using XMLStreamReader
-	// since SAX doesnt support this until Locator2 (in sax 2.02)
-	// only do reparse at request to keep the peeker fast
-	// ********************************************************
+    public void startDocument()
+            throws SAXException {
+    }
 
-	public String getXMLVersion() {
-		try {
-			if (xer == null)
-				readStream();
-			return xmlVersion;
-		} catch (XMLStreamException e) {
-			return null;
-		}
-	}
+    public void endDocument() throws SAXException {
+    }
 
-	public String getEncoding() {
-		try {
-			if (xer == null)
-				readStream();
-			return xmlEncoding;
-		} catch (XMLStreamException e) {
-			return null;
-		}
-	}
+    public void startPrefixMapping(String prefix,
+            String uri) throws SAXException {
+    }
 
-	public boolean getStandalone() {
-		try {
-			if (xer == null)
-				readStream();
-			return xmlStandalone.equals("true")
-					? true
-					: false;
-		} catch (XMLStreamException e) {
-			return false;
-		}
-	}
+    public void endPrefixMapping(String prefix)
+            throws SAXException {
+    }
 
-	private void readStream() throws XMLStreamException {
-		try {
-			
-			if(staxParserProperties == null){
-				staxParserProperties = new HashMap();				
-				staxParserProperties.put("javax.xml.stream.isCoalescing",  Boolean.TRUE);
-				staxParserProperties.put("javax.xml.stream.isNamespaceAware",  Boolean.TRUE);
-				staxParserProperties.put("javax.xml.stream.isReplacingEntityReferences",  Boolean.FALSE);
-				staxParserProperties.put("javax.xml.stream.isSupportingExternalEntities",  Boolean.FALSE);
-				staxParserProperties.put("javax.xml.stream.isValidating",  Boolean.FALSE);
-				staxParserProperties.put("javax.xml.stream.supportDTD",  Boolean.FALSE);
-			}	
-			
-			XMLInputFactory xif = StAXInputFactoryPool.getInstance().acquire(staxParserProperties); 
-			xif.setXMLResolver(new StaxEntityResolver(CatalogEntityResolver.getInstance()));																
-			XMLStreamReader xer  = xif.createXMLStreamReader(new StreamSource(currentURL.openStream()));
-			while (true) {
-				if (xer.getEventType() == XMLStreamConstants.START_DOCUMENT) {
-					readProps();
-					xer.close();
-					break;
-				}
-				int event = xer.next();
-				if (event == XMLStreamConstants.END_DOCUMENT) {
-					xer.close();
-					break;
-				} else if (event == XMLStreamConstants.START_DOCUMENT
-						|| event == XMLStreamConstants.START_ELEMENT) {
-					readProps();
-					xer.close();
-					break;
-				}
-			}
-		} catch (Exception e) {
-			throw new XMLStreamException(e.getMessage(), e);
-		}
-	}
+    public void characters(char[] ch, int start,
+            int length) throws SAXException {
+    }
 
-	private void readProps() {
-		xmlVersion = xer.getVersion();
-		xmlEncoding = xer.getCharacterEncodingScheme();
-		boolean sa = xer.isStandalone();
-		if (sa) {
-			xmlStandalone = "true";
-		} else {
-			xmlStandalone = "false";
-		}
-	}
+    public void ignorableWhitespace(char[] ch,
+            int start, int length)
+            throws SAXException {
+    }
 
-	public void endElement(String uri,String localName, String qName)throws SAXException {}
-	public void setDocumentLocator(Locator locator) {}
-	public void startDocument()throws SAXException {}
-	public void endDocument() throws SAXException {}
-	public void startPrefixMapping(String prefix,String uri) throws SAXException {}
-	public void endPrefixMapping(String prefix)throws SAXException {}
-	public void characters(char[] ch, int start,int length) throws SAXException {}
-	public void ignorableWhitespace(char[] ch,int start, int length)throws SAXException {}
-	public void processingInstruction(String target, String data)throws SAXException {}
-	public void skippedEntity(String name)throws SAXException {}
-	public void startDTD(String name,String publicId, String systemId)throws SAXException {}
-	public void endDTD() throws SAXException {}
-	public void startEntity(String name)throws SAXException {}
-	public void endEntity(String name)throws SAXException {}
-	public void startCDATA() throws SAXException {}
-	public void endCDATA() throws SAXException {}
-	public void comment(char[] ch, int start,int length) throws SAXException {}
-	public void warning(SAXParseException exception)throws SAXException {}
-	public void error(SAXParseException exception)throws SAXException {}
-	public void fatalError(SAXParseException exception)throws SAXException {}
-	public void notationDecl(String name,String publicId, String systemId)throws SAXException {}
-	public void unparsedEntityDecl(String name,String publicId, String systemId,String notationName)throws SAXException {}
-	
+    public void processingInstruction(
+            String target, String data)
+            throws SAXException {
+    }
+
+    public void skippedEntity(String name)
+            throws SAXException {
+    }
+
+    public void startDTD(String name,
+            String publicId, String systemId)
+            throws SAXException {
+    }
+
+    public void endDTD() throws SAXException {
+    }
+
+    public void startEntity(String name)
+            throws SAXException {
+    }
+
+    public void endEntity(String name)
+            throws SAXException {
+    }
+
+    public void startCDATA() throws SAXException {
+    }
+
+    public void endCDATA() throws SAXException {
+    }
+
+    public void comment(char[] ch, int start,
+            int length) throws SAXException {
+    }
+
+    public void warning(
+            SAXParseException exception)
+            throws SAXException {
+    }
+
+    public void error(SAXParseException exception)
+            throws SAXException {
+    }
+
+    public void fatalError(
+            SAXParseException exception)
+            throws SAXException {
+    }
+
+    public void notationDecl(String name,
+            String publicId, String systemId)
+            throws SAXException {
+    }
+
+    public void unparsedEntityDecl(String name,
+            String publicId, String systemId,
+            String notationName)
+            throws SAXException {
+    }
+
+    public InputSource resolveEntity(
+            String publicId, String systemId)
+            throws SAXException, IOException {
+        if (publicId != null) {
+            this.firstPublicId = publicId;
+        }
+        if (systemId != null) {
+            this.firstSystemId = systemId;
+        }
+        return CatalogEntityResolver.getInstance().resolveEntity(publicId, systemId);
+    }
+
+    // ********************************************************
+    // below methods that force reparse using XMLStreamReader
+    // since SAX doesnt support this until Locator2 (in sax 2.02)
+    // only do reparse at request to keep the peeker fast
+    // ********************************************************
+
+    public String getXMLVersion() {
+        try {
+            if (xer == null)
+                readStream();
+            return xmlVersion;
+        } catch (XMLStreamException e) {
+            return null;
+        }
+    }
+
+    public String getEncoding() {
+        try {
+            if (xer == null)
+                readStream();
+            return xmlEncoding;
+        } catch (XMLStreamException e) {
+            return null;
+        }
+    }
+
+    public boolean getStandalone() {
+        try {
+            if (xer == null)
+                readStream();
+            return xmlStandalone.equals("true")
+                    ? true
+                    : false;
+        } catch (XMLStreamException e) {
+            return false;
+        }
+    }
+
+    private void readStream()
+            throws XMLStreamException {
+        try {
+            if (xif == null) {
+                xif = XMLInputFactory.newInstance();
+                xif.setProperty(XMLInputFactory.IS_COALESCING, Boolean.TRUE);
+                xif.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, Boolean.TRUE);
+                xif.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, Boolean.FALSE);
+                xif.setProperty(XMLInputFactory.IS_REPLACING_ENTITY_REFERENCES, Boolean.FALSE);
+                xif.setProperty(XMLInputFactory.IS_VALIDATING, Boolean.FALSE);
+                xif.setProperty(XMLInputFactory.SUPPORT_DTD, Boolean.FALSE);
+                xif.setXMLResolver(new StaxEntityResolver(CatalogEntityResolver.getInstance()));
+            }
+            File f = new File(currentURI);
+            xer = xif.createXMLStreamReader(new FileInputStream(f));            
+            while (true) {
+                if (xer.getEventType() == XMLStreamConstants.START_DOCUMENT) {
+                    readProps();
+                    xer.close();
+                    break;
+                }               
+                
+                int event = xer.next();
+                if (event == XMLStreamConstants.END_DOCUMENT) {
+                    xer.close();
+                    break;
+                } else if (event == XMLStreamConstants.START_DOCUMENT
+                        ||event == XMLStreamConstants.START_ELEMENT) {
+                    readProps();
+                    xer.close();
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            throw new XMLStreamException(e.getMessage(), e);
+        }
+    }
+
+    private void readProps() {
+        xmlVersion = xer.getVersion();
+        xmlEncoding = xer.getCharacterEncodingScheme();
+        boolean sa = xer.isStandalone();
+        if (sa) {
+            xmlStandalone = "true";
+        } else {
+            xmlStandalone = "false";
+        }
+    }
+
 }
