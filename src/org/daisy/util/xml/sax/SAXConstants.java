37059b3c2413ab73955b2998abc00421668201db
Markus Gylling <markus.gylling@gmail.com>
2006-07-26 12:55:14 +0000

    initial commit

---
+++ src/org/daisy/util/xml/sax/SAXConstants.java
@@ -0,0 +1,33 @@
+package org.daisy.util.xml.sax;
+
+/**
+ * 
+ * @author Markus Gylling
+ */
+
+public class SAXConstants {
+
+	private SAXConstants() {} 
+		
+	public static final String SAX_FEATURE_EXTERNAL_GENERAL_ENTITIES = "http://xml.org/sax/features/external-general-entities";
+	public static final String SAX_FEATURE_EXTERNAL_PARAMETER_ENTITIES="http://xml.org/sax/features/external-parameter-entities";
+	public static final String SAX_FEATURE_IS_STANDALONE="http://xml.org/sax/features/is-standalone";
+	public static final String SAX_FEATURE_LEXICAL_HANDLER_PARAMETER_ENTITIES="http://xml.org/sax/features/lexical-handler/parameter-entities";
+	public static final String SAX_FEATURE_NAMESPACES="http://xml.org/sax/features/namespaces";
+	public static final String SAX_FEATURE_NAMESPACE_PREFIXES="http://xml.org/sax/features/namespace-prefixes";
+	public static final String SAX_FEATURE_RESOLVE_DTD_URIS="http://xml.org/sax/features/resolve-dtd-uris";
+	public static final String SAX_FEATURE_STRING_INTERNING="http://xml.org/sax/features/string-interning";
+	public static final String SAX_FEATURE_UNICODE_NORMALIZATION_CHECKING="http://xml.org/sax/features/unicode-normalization-checking";
+	public static final String SAX_FEATURE_USE_ATTRIBUTES2="http://xml.org/sax/features/use-attributes2";
+	public static final String SAX_FEATURE_USE_LOCATOR2="http://xml.org/sax/features/use-locator2";
+	public static final String SAX_FEATURE_USE_ENTITY_RESOLVER2="http://xml.org/sax/features/use-entity-resolver2";
+	public static final String SAX_FEATURE_VALIDATION="http://xml.org/sax/features/validation";
+	public static final String SAX_FEATURE_XMLNS_URIS="http://xml.org/sax/features/xmlns-uris";
+	public static final String SAX_FEATURE_XML_1_1="http://xml.org/sax/features/xml-1.1";
+	 
+	public static final String SAX_PROPERTY_DECLARATION_HANDLER="http://xml.org/sax/properties/declaration-handler";
+	public static final String SAX_PROPERTY_LEXICAL_HANDLER="http://xml.org/sax/properties/lexical-handler";
+	
+	public static final String APACHE_FEATURE_ALLOW_JAVA_ENCODINGS="http://apache.org/xml/features/allow-java-encodings";
+	
+}
