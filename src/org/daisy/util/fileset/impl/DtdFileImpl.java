a4efc34b8679a661238ddc3f38587e82313ecf7e
Markus Gylling <markus.gylling@gmail.com>
2006-08-14 11:24:52 +0000

    changes in interface placement

--- src/org/daisy/util/fileset/impl/DtdFileImpl.java
+++ src/org/daisy/util/fileset/impl/DtdFileImpl.java
@@ -3,10 +3,12 @@ package org.daisy.util.fileset.impl;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
-import org.daisy.util.fileset.interfaces.xml.DtdFile;
+
+import org.daisy.util.fileset.interfaces.SchemaFile;
+import org.daisy.util.fileset.interfaces.schema.DtdFile;
 
 
-final class DtdFileImpl extends FilesetFileImpl implements DtdFile {
+final class DtdFileImpl extends FilesetFileImpl implements DtdFile, SchemaFile {
 
 	DtdFileImpl(URI uri, String mimeString) throws IOException, FileNotFoundException {
 		super(uri, mimeString);
