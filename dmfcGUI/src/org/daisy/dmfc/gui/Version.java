69dae330793869dfe061f318cc3fe62ea773c644
lauriesherve <lauriesherve@27897e58-4529-0410-8da3-928601d0d17f>
2006-08-14 19:05:36 +0000

    Added version class to gui

---
+++ dmfcGUI/src/org/daisy/dmfc/gui/Version.java
@@ -0,0 +1,14 @@
+package org.daisy.dmfc.gui;
+
+/**
+ * Exposes global version information on the whole org.daisy.util package
+ * @author Markus Gylling
+ */
+public class Version {
+
+	private static final String VERSION = "1.0.0";
+	
+	public static String getVersion() {
+		return VERSION;
+	}
+}
