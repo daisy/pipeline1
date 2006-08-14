595de8d49d1309136456c3e5624c3b1b01a62e6b
Markus Gylling <markus.gylling@gmail.com>
2006-08-14 18:58:03 +0000

    initial commit

---
+++ src/org/daisy/util/fileset/interfaces/SchemaFile.java
@@ -0,0 +1,10 @@
+package org.daisy.util.fileset.interfaces;
+
+/**
+ * Empty interface that all <code>FilesetFile</code>s that represent
+ * schemas (DTDs, RelaxNGs, etc) must implement. 
+ * @author Markus Gylling
+ */
+public interface SchemaFile {
+
+}
