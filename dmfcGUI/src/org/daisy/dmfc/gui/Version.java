8885a0f352b3c33d503b42a179714d47999027d2
lauriesherve <lauriesherve@27897e58-4529-0410-8da3-928601d0d17f>
2006-08-23 21:22:39 +0000

    Version changed to date

--- dmfcGUI/src/org/daisy/dmfc/gui/Version.java
+++ dmfcGUI/src/org/daisy/dmfc/gui/Version.java
@@ -1,12 +1,12 @@
 package org.daisy.dmfc.gui;
 
 /**
- * Exposes global version information on the whole org.daisy.util package
+ * Exposes global version information on the whole org.daisy.dmfc.gui package
  * @author Markus Gylling
  */
 public class Version {
 
-	private static final String VERSION = "1.0.0";
+	private static final String VERSION = "2006-08-23";
 	
 	public static String getVersion() {
 		return VERSION;
