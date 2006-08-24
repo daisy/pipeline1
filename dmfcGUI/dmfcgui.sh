1b641558a57adddb6ae4e0ffd200e88c1eb51ae5
linusericson <linusericson@27897e58-4529-0410-8da3-928601d0d17f>
2006-08-24 21:19:05 +0000

    launcher script for unix aded

---
+++ dmfcGUI/dmfcgui.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#######################################################################
+# Simple DMFC GUI launch script for Unix systems
+# Linus Ericson 2006
+#######################################################################
+
+JAVA=java
+
+BASE=`dirname $0`
+
+CP=
+for jar in `ls $BASE/lib/*.jar`; do
+    CP="$jar":$CP
+done
+
+CP=$CP:$BASE/org.daisy.dmfc.gui.jar:$BASE/dmfc/lib/tagsoup-1.0rc4.jar:$BASE/dmfc/lib/org.daisy.util.jar:$BASE/dmfc/bin:$BASE/jython/jython.jar
+
+#echo $JAVA -Djava.library.path=$BASE -classpath $CP org.daisy.dmfc.gui.DMFCMain
+$JAVA -Djava.library.path=$BASE -classpath $CP org.daisy.dmfc.gui.DMFCMain
