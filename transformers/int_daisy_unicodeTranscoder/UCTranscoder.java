0ae683c3536df0ae430ada26987e01abcb1e4a2e
Markus Gylling <markus.gylling@gmail.com>
2006-09-03 15:39:09 +0000

    major extensions (see doc.html)

--- transformers/int_daisy_unicodeTranscoder/UCTranscoder.java
+++ transformers/int_daisy_unicodeTranscoder/UCTranscoder.java
@@ -21,9 +21,9 @@ package int_daisy_unicodeTranscoder;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.charset.Charset;
+import java.nio.charset.IllegalCharsetNameException;
+import java.nio.charset.UnsupportedCharsetException;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
@@ -49,23 +49,24 @@ import org.daisy.util.fileset.manipulation.FilesetManipulatorListener;
 import org.daisy.util.fileset.manipulation.manipulators.XMLEventFeeder;
 import org.daisy.util.fileset.manipulation.manipulators.XMLEventValueConsumer;
 import org.daisy.util.fileset.manipulation.manipulators.XMLEventValueExposer;
-import org.daisy.util.i18n.CharsetDetector;
+import org.daisy.util.i18n.CharUtils;
 import org.daisy.util.i18n.UCharReplacer;
 import org.daisy.util.xml.stax.ContextStack;
 
 /**
  * Performs character set transcoding on all XML documents in a fileset, roundtripping 
- * through a Unicode representation. Can optionally substitute characters in the XML file 
- * with replacement strings.
+ * through a Unicode representation. Can optionally replace characters in the XML file 
+ * with substitution strings.
  * @author Markus Gylling
  */
 public class UCTranscoder extends Transformer implements FilesetManipulatorListener, XMLEventValueConsumer {
 	
-	private int xmlFileCount = 0; //for progress
-	private int processedCount = 0; //for progress	
-	private UCharReplacer ucr = null; //if null at access, dont perform ucharacter replacement
-	private boolean replaceOnAttributeValues = false; //perform UCharReplacement on textnodes only					
-	private Charset outputEncoding = null;  //if null at access, maintain input file encoding
+	private int mXmlFileCount = 0; 								//for progress
+	private int mProcessedCount = 0; 							//for progress	
+	private UCharReplacer mUCharReplacer = null; 				//if null at access, dont perform ucharacter replacement
+	private boolean mPerformUCharSubstitution = false; 			//whether to perform UCharReplacement 
+	private boolean mSubstituteInAttributeValues = false; 		//perform UCharReplacement on textnodes only					
+	private Charset mOutputEncoding = null;  					//if null at access, maintain input file encoding
 		
     public  UCTranscoder (InputListener inListener, Set eventListeners, Boolean isInteractive) {
         super(inListener, eventListeners, isInteractive);        
@@ -74,67 +75,133 @@ public class UCTranscoder extends Transformer implements FilesetManipulatorListe
 	protected boolean execute(Map parameters) throws TransformerRunException {
 		
 		try{
+			//TODO set system properties; StAX Writer
+			
 			String param;
 			
 			FilesetManipulator fm = new FilesetManipulator();
+			
 			//implement FilesetManipulatorListener
 			fm.setListener(this);
+			
 			//set input fileset
-			fm.setInputFileset(new File((String)parameters.remove("inXML")).toURI());
+			fm.setInputFileset(new File((String)parameters.remove("input")).toURI());
+			
 			//set destination
-			fm.setOutputFolder((EFolder)FileUtils.createDirectory(new EFolder((String)parameters.remove("outDir"))));
+			fm.setOutputFolder((EFolder)FileUtils.createDirectory(new EFolder((String)parameters.remove("output"))));
+			
 			//set restriction, only listen to XmlFile
 			fm.setFileTypeRestriction(XmlFile.class);
+			
 			//determine what charset encoding to use in output 
 			param = ((String)parameters.remove("outputEncoding"));
-			if(param!=null) outputEncoding = Charset.forName(param);				 									
-			//configure the UCharReplacer			
-			configureUCR(parameters);		
+			if(param!=null) mOutputEncoding = Charset.forName(param);	
+		
 			//check user settable node restriction
-			this.replaceOnAttributeValues = ((String)parameters.remove("replaceOnAttributeValues")).equals("true");
+			this.mSubstituteInAttributeValues = ((String)parameters.remove("substituteInAttributeValues")).equals("true");
+			
+			//configure the UCharReplacer, if used		
+			mPerformUCharSubstitution = ((String)parameters.remove("performCharacterSubstitution")).equals("true");
+			if(mPerformUCharSubstitution) configureUCR(parameters);
+
 			//set a counter for progress sake
-			this.xmlFileCount = getXmlFileCount(fm.getInputFileset());
+			this.mXmlFileCount = getXmlFileCount(fm.getInputFileset());
+			
 			//run...
 			fm.iterate();
+			
 			//done.
-			this.sendMessage(Level.FINE, "Completed transcoding of " + processedCount + " files.");
+			this.sendMessage(Level.FINE, "Completed transcoding of " + mProcessedCount + " files.");
+			
+			//print any transliteration info and warnings
+			if(mUCharReplacer!=null) {
+				//number of successful table hits
+				if(!mUCharReplacer.getTranslationTables().isEmpty()) {
+					this.sendMessage(Level.INFO, "Performed " 
+							+ mUCharReplacer.getTranslationTableHitCount() 
+							+ " character substitions using loaded tables");
+				}
+				
+				//warnings (table failures)
+				Integer codePoint;
+				String value;
+				Map map = mUCharReplacer.getTranslationTableFailures();
+				for (Iterator iter = map.keySet().iterator(); iter.hasNext();) {
+					codePoint = (Integer) iter.next();					
+					value = (String)map.get(codePoint);
+					StringBuilder sb = new StringBuilder(127);
+					sb.append("No user provided replacement text found for ");
+					sb.append(CharUtils.unicodeHexEscape(codePoint.intValue()));
+					sb.append(" [");
+					sb.append(String.copyValueOf(Character.toChars(codePoint.intValue())));
+					sb.append(']');
+				    if(value!=null) {
+				    	sb.append(". Using fallback '").append(value).append("' instead.");
+				    }else{
+				    	sb.append(". Input character forwarded to output.");
+				    }						
+				    this.sendMessage(Level.WARNING, sb.toString());
+				}
+							
+			}
 			
 		} catch (Exception e) {
 			this.sendMessage(Level.SEVERE, e.getMessage());
 			throw new TransformerRunException(e.getMessage(),e);
+		} finally {
+			//TODO reset system properties: StAX Writer
 		}
 			
 		return true;
 	}
 
-	private void configureUCR(Map parameters) throws IOException, MalformedURLException {
-		
-		String param = ((String)parameters.remove("translationTable"));
+	/**
+	 * Configure the UCharReplacer object.
+	 */
+	private void configureUCR(Map parameters) throws IllegalCharsetNameException, UnsupportedCharsetException, UnsupportedOperationException {		
+		String param = ((String)parameters.remove("substitutionTables"));
 		if(param!=null){
-			ucr = new UCharReplacer();
+			mUCharReplacer = new UCharReplacer();
 			String[] tables = param.split(",");
 			for (int i = 0; i < tables.length; i++) {
 				File t = new File(tables[i].trim());
 				if(t.exists()) {
 					try{
-						ucr.addTranslationTable(t.toURL(),getEncoding(t));
+						mUCharReplacer.addSubstitutionTable(t.toURI().toURL());
 					}catch (Exception e) {
 						this.sendMessage(Level.WARNING,"Translation table " + t.getPath() + " exception: " + e.getMessage());
 					}	
 				}else{
 					this.sendMessage(Level.WARNING,"Translation table " + t.getPath() + " could not be found");
 				}				
-			}	
-			param = (String) parameters.remove("fallbackToUCD");
-			ucr.setFallbackToUCD(param.equals("true"));		
-			
-			//if we havent got an ok configuration of ucr
-			if(!ucr.hasUserTables() && !ucr.getFallbackToUCD()) {
-				this.sendMessage(Level.WARNING,"No translation tables loaded");
-				ucr=null;
-			}
-			
-		} //if != null
+			} //for				
+		} //if param != null
+		
+		param = (String)parameters.remove("fallbackToUCD");		
+		mUCharReplacer.setFallbackState(mUCharReplacer.FALLBACK_USE_UCD_NAMES, param.equals("true"));
+		
+		param = (String)parameters.remove("fallbackToLatinTransliteration");		
+		mUCharReplacer.setFallbackState(mUCharReplacer.FALLBACK_TRANSLITERATE_ANY_TO_LATIN, param.equals("true"));
+		
+		param = (String)parameters.remove("fallbackToNonSpacingMarkRemovalTransliteration");		
+		mUCharReplacer.setFallbackState(mUCharReplacer.FALLBACK_TRANSLITERATE_REMOVE_NONSPACING_MARKS, param.equals("true"));
+		
+		param = (String)parameters.remove("excludeFromSubstitution");
+		if(param!=null) {			
+			Charset chrs = Charset.forName(param);
+			mUCharReplacer.setExclusionRepertoire(chrs);			
+		}
+				
+		//if we havent got an ok configuration of UCharReplacer
+		if((mUCharReplacer.getTranslationTables().isEmpty()) 
+				&& (!mUCharReplacer.getFallbackState(mUCharReplacer.FALLBACK_USE_UCD_NAMES)
+						&&!mUCharReplacer.getFallbackState(mUCharReplacer.FALLBACK_TRANSLITERATE_ANY_TO_LATIN)
+						&&!mUCharReplacer.getFallbackState(mUCharReplacer.FALLBACK_TRANSLITERATE_REMOVE_NONSPACING_MARKS)
+						)) {
+			this.sendMessage(Level.WARNING,"No translation tables loaded, and no fallbacks enabled");
+			mUCharReplacer=null;
+		}
+
 	}
 	
 	/**
@@ -146,22 +213,20 @@ public class UCTranscoder extends Transformer implements FilesetManipulatorListe
 		try {
 			//transformer generics
 			this.checkAbort();
-			processedCount++;
-			this.progress(processedCount/xmlFileCount);
-									
-			//smilfiles are only empty elements so abort if not replaceOnAttributeValues to save time
-			if(!this.replaceOnAttributeValues && file instanceof SmilFile) return null;	
-			
+			mProcessedCount++;
+			this.progress(mProcessedCount/mXmlFileCount);
+														
 			//determine manipulation type and return
 			FilesetFileManipulator ffm;			
-			if(ucr!=null) { 
+			//smilfiles are only empty elements so no replace if not replaceOnAttributeValues to save time
+			if(mUCharReplacer!=null &&(!(!this.mSubstituteInAttributeValues && file instanceof SmilFile))) { 
 				//return a value exposer, use the outcharset constructor
-				XMLEventValueExposer xeve = new XMLEventValueExposer(this,true,this.outputEncoding);
-				if(!this.replaceOnAttributeValues) xeve.setEventTypeRestriction(XMLEvent.CHARACTERS);				
+				XMLEventValueExposer xeve = new XMLEventValueExposer(this,true,mOutputEncoding);
+				if(!this.mSubstituteInAttributeValues) xeve.setEventTypeRestriction(XMLEvent.CHARACTERS);				
 				ffm = xeve;
 			}else{
 				//return a feeder, use the outcharset constructor  
-				ffm = new XMLEventFeeder(this.outputEncoding);				
+				ffm = new XMLEventFeeder(mOutputEncoding);				
 			}			
 			return ffm;
 						
@@ -186,7 +251,7 @@ public class UCTranscoder extends Transformer implements FilesetManipulatorListe
 	 */
 	public String nextValue(String value, ContextStack context) {		
 		try{						
-			return ucr.toReplacementString(value);			
+			return mUCharReplacer.replace(value).toString();			
 		}catch (Exception e) {
 			this.sendMessage(Level.WARNING,e.getMessage());
 			return null;
@@ -205,12 +270,4 @@ public class UCTranscoder extends Transformer implements FilesetManipulatorListe
 		return count;
 	}
 	
-	private String getEncoding(File translationTable) throws MalformedURLException, IOException {
-		 CharsetDetector det = new CharsetDetector();
-		 String charset = det.detect(translationTable.toURL());
-		 if(null==charset) {
-		 	charset = det.getProbableCharsetUsingLocale();
-		 }
-		 return charset;
-	}
 }
