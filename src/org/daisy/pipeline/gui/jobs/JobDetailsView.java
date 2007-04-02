22a8bb3e6e95c7b6ee4ff5bfe57b8bfdcb15f2dd
romaindeltour <romaindeltour@27897e58-4529-0410-8da3-928601d0d17f>
2007-04-02 10:27:35 +0000

    Added the strawman of the view that shows job details (task list + progress)

---
+++ src/org/daisy/pipeline/gui/jobs/JobDetailsView.java
@@ -0,0 +1,35 @@
+package org.daisy.pipeline.gui.jobs;
+
+import org.daisy.pipeline.gui.Fake;
+import org.daisy.pipeline.gui.tasks.TaskListContentProvider;
+import org.daisy.pipeline.gui.tasks.TaskListViewer;
+import org.eclipse.jface.viewers.StructuredViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+public class JobDetailsView extends ViewPart {
+
+    public static final String ID = "org.daisy.pipeline.gui.views.jobDetails";
+
+    private StructuredViewer viewer;
+
+    public JobDetailsView() {
+
+    }
+
+    @Override
+    public void createPartControl(Composite parent) {
+        viewer = new TaskListViewer(parent, SWT.SINGLE);
+        viewer.setContentProvider(new TaskListContentProvider());
+        viewer.setInput(Fake.getJob());
+        getSite().setSelectionProvider(viewer);
+    }
+
+    @Override
+    public void setFocus() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
