a9b42abf9ad29141d7d8975b2ec5392a4fd988e6
romaindeltour <romaindeltour@27897e58-4529-0410-8da3-928601d0d17f>
2007-04-02 10:26:10 +0000

    Added GUI classes for the job details view (list of tasks + progress)

---
+++ src/org/daisy/pipeline/gui/tasks/TaskItem.java
@@ -0,0 +1,186 @@
+package org.daisy.pipeline.gui.tasks;
+
+import org.daisy.dmfc.core.script.Job;
+import org.daisy.pipeline.gui.IIconsKeys;
+import org.daisy.pipeline.gui.PipelineGuiPlugin;
+import org.daisy.pipeline.gui.util.swt.CompositeItem;
+import org.daisy.pipeline.gui.util.swt.CompositeList;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.ProgressBar;
+import org.eclipse.swt.widgets.ToolBar;
+import org.eclipse.swt.widgets.ToolItem;
+
+/**
+ * @author Romain Deltour
+ * 
+ */
+public class TaskItem extends CompositeItem {
+    private static String IK_DOC_BUTTON = "org.daisy.pipeline.gui.jobdetails.DOC_BUTTON";
+    private static String IK_IDLE = "org.daisy.pipeline.gui.jobdetails.IDLE";
+    private static String IK_RUNNING = "org.daisy.pipeline.gui.jobdetails.RUNNING";
+    private static String IK_FINISHED = "org.daisy.pipeline.gui.jobdetails.FINISHED";
+    static {
+        PipelineGuiPlugin.getDefault().getImageRegistry().put(IK_DOC_BUTTON,
+                PipelineGuiPlugin.getIcon(IIconsKeys.HELP_BROWSER));
+        PipelineGuiPlugin.getDefault().getImageRegistry()
+                .put(
+                        IK_IDLE,
+                        PipelineGuiPlugin
+                                .getImageDescriptor("icons/progress_task.gif"));
+        PipelineGuiPlugin.getDefault().getImageRegistry().put(
+                IK_RUNNING,
+                PipelineGuiPlugin
+                        .getImageDescriptor("icons/progress-indicator.gif"));
+        PipelineGuiPlugin.getDefault().getImageRegistry().put(IK_FINISHED,
+                PipelineGuiPlugin.getImageDescriptor("icons/tick.png"));
+    }
+    private Label iconLabel;
+    private Label nameLabel;
+    private Label timeLabel;
+    private ProgressBar progressBar;
+    private ToolBar toolBar;
+    private ToolItem docButton;
+    private Job.State lastState;
+
+    public TaskItem(CompositeList<TaskItem> parent, int style, int index) {
+        super(parent, style, index);
+        createChildren();
+        setLayoutsForNoProgress();
+        setLayoutData(new GridData(SWT.FILL, SWT.NONE, true, false));
+    }
+
+    @Override
+    public void refresh() {
+        super.refresh();
+        if (!(getData() instanceof TaskInfo)) {
+            return;
+        }
+        TaskInfo info = (TaskInfo) getData();
+        nameLabel.setText(info.getDescription());
+        progressBar.setSelection(((Double) (info.getProgress() * 100))
+                .intValue());
+        timeLabel.setText(getTimeText(info));
+        Job.State state = info.getSate();
+        if (state != lastState) {
+            lastState = state;
+            iconLabel.setImage(getImage(state));
+        }
+    }
+
+    private String getTimeText(TaskInfo info) {
+        String text;
+        switch (info.getSate()) {
+        case RUNNING:
+            text = info.getLeftTime()+" ms left";
+            break;
+        case FINISHED:
+            text = "Done in "+info.getTotalTime()+" ms";
+            break;
+        default:
+            text="";
+            break;
+        }
+        return text;
+    }
+
+    /**
+     * @return
+     */
+    private Image getImage(Job.State state) {
+        Image image = null;
+        switch (state) {
+        case IDLE:
+            image = PipelineGuiPlugin.getDefault().getImageRegistry().get(
+                    IK_IDLE);
+            break;
+        case RUNNING:
+            image = PipelineGuiPlugin.getDefault().getImageRegistry().get(
+                    IK_RUNNING);
+            break;
+        case FINISHED:
+            image = PipelineGuiPlugin.getDefault().getImageRegistry().get(
+                    IK_FINISHED);
+            break;
+        default:
+            break;
+        }
+        return image;
+    }
+
+    private void createChildren() {
+        FormData formData;
+        FormLayout layout = new FormLayout();
+        setLayout(layout);
+
+        // Create the icon label on the left
+        iconLabel = new Label(this, SWT.NONE);
+        formData = new FormData();
+        formData.top = new FormAttachment(0, 5);
+        formData.left = new FormAttachment(0, 5);
+        iconLabel.setLayoutData(formData);
+
+        // Create action bar on the right
+        toolBar = new ToolBar(this, SWT.FLAT);
+        // TODO check if toolBar.setCursor() is necessary
+        toolBar.setCursor(getDisplay().getSystemCursor(SWT.CURSOR_ARROW));
+        formData = new FormData();
+        formData.top = new FormAttachment(0, 5);
+        formData.right = new FormAttachment(100, -5);
+        toolBar.setLayoutData(formData);
+
+        // Create name label
+        nameLabel = new Label(this, SWT.NONE);
+        formData = new FormData();
+        formData.top = new FormAttachment(0, 5);
+        formData.left = new FormAttachment(iconLabel, 5);
+        formData.right = new FormAttachment(toolBar, -5);
+        nameLabel.setLayoutData(formData);
+
+        // Create doc button
+        docButton = new ToolItem(toolBar, SWT.PUSH);
+        docButton.setToolTipText("Show documentation");
+        docButton.setImage(PipelineGuiPlugin.getDefault().getImageRegistry()
+                .get(IK_DOC_BUTTON));
+        docButton.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                showDoc();
+            }
+        });
+        
+        // Create time info label
+        timeLabel = new Label(this,SWT.NONE);
+        formData = new FormData();
+        formData.top = new FormAttachment(nameLabel, 5);
+        formData.left = new FormAttachment(0, 5);
+        formData.right = new FormAttachment(100, -5);
+        timeLabel.setLayoutData(formData);
+        
+        // Create Progress Bar
+        progressBar = new ProgressBar(this, SWT.HORIZONTAL);
+        progressBar.setMinimum(0);
+        progressBar.setMaximum(100);
+        formData = new FormData();
+        formData.top = new FormAttachment(timeLabel, 5);
+        formData.left = new FormAttachment(0, 5);
+        formData.right = new FormAttachment(100, -5);
+        progressBar.setLayoutData(formData);
+    }
+
+    private void setLayoutsForNoProgress() {
+
+    }
+
+    private void showDoc() {
+        // TODO implem showDoc()
+        System.out.println("show doc");
+    }
+}
---
+++ src/org/daisy/pipeline/gui/tasks/TaskListContentProvider.java
@@ -0,0 +1,167 @@
+package org.daisy.pipeline.gui.tasks;
+
+import java.util.ArrayList;
+import java.util.EventObject;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.daisy.dmfc.core.event.BusListener;
+import org.daisy.dmfc.core.event.EventBus;
+import org.daisy.dmfc.core.event.TransformerProgressChangeEvent;
+import org.daisy.dmfc.core.event.TransformerStateChangeEvent;
+import org.daisy.dmfc.core.script.Job;
+import org.daisy.dmfc.core.script.Task;
+import org.daisy.dmfc.core.transformer.Transformer;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.StructuredViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.progress.WorkbenchJob;
+
+/**
+ * @author Romain Deltour
+ * 
+ */
+public class TaskListContentProvider implements IStructuredContentProvider,
+        BusListener {
+
+    private List<TaskInfo> infos;
+    private TaskInfo lastInfo;
+    private Refresher refresher;
+
+    public TaskListContentProvider() {
+        infos = new ArrayList<TaskInfo>();
+        EventBus.getInstance().subscribe(this,
+                TransformerProgressChangeEvent.class);
+        EventBus.getInstance().subscribe(this,
+                TransformerStateChangeEvent.class);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.jface.viewers.IContentProvider#dispose()
+     */
+    public void dispose() {
+        EventBus.getInstance().unsubscribe(this,
+                TransformerProgressChangeEvent.class);
+        EventBus.getInstance().unsubscribe(this,
+                TransformerStateChangeEvent.class);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.jface.viewers.IStructuredContentProvider#getElements(java.lang.Object)
+     */
+    public Object[] getElements(Object inputElement) {
+        return infos.toArray();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.jface.viewers.IContentProvider#inputChanged(org.eclipse.jface.viewers.Viewer,
+     *      java.lang.Object, java.lang.Object)
+     */
+    public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+        if (newInput == null || !(newInput instanceof Job)) {
+            return;
+        }
+        infos.clear();
+        Job job = (Job) newInput;
+        List<Task> tasks = job.getScript().getTasks();
+        for (Task task : tasks) {
+            infos.add(new TaskInfo(task.getTransInfo()));
+        }
+        refresher = new Refresher((StructuredViewer) viewer);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.daisy.dmfc.core.event.BusListener#recieved(java.util.EventObject)
+     */
+    public void recieved(EventObject event) {
+        TaskInfo info = null;
+        if (event instanceof TransformerStateChangeEvent) {
+            TransformerStateChangeEvent tce = (TransformerStateChangeEvent) event;
+            Transformer trans = (Transformer) tce.getSource();
+            info = getInfo(trans);
+            if (info != null) {
+                switch (tce.getState()) {
+                case STARTED:
+                    info.setState(Job.State.RUNNING);
+                    break;
+                case STOPPED:
+                    info.setState(Job.State.FINISHED);
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+        if (event instanceof TransformerProgressChangeEvent) {
+            TransformerProgressChangeEvent tpce = (TransformerProgressChangeEvent) event;
+            Transformer trans = (Transformer) tpce.getSource();
+            info = getInfo(trans);
+            if (info != null) {
+                info.setProgress(tpce.getProgress());
+            }
+        }
+        if (info != null) {
+            refresher.refresh(info);
+        }
+    }
+
+    private TaskInfo getInfo(Transformer trans) {
+        String name = trans.getTransformerInfo().getName();
+        if (lastInfo != null && lastInfo.getName().equals(name)) {
+            return lastInfo;
+        }
+        for (TaskInfo info : infos) {
+            if (info.getName().equals(name)) {
+                lastInfo = info;
+                return info;
+            }
+        }
+        return null;
+    }
+
+    private class Refresher {
+        private StructuredViewer viewer;
+        private List<TaskInfo> refreshInfos;
+        private org.eclipse.core.runtime.jobs.Job refreshJob;
+
+        public Refresher(StructuredViewer aviewer) {
+            viewer = aviewer;
+            refreshInfos = new LinkedList<TaskInfo>();
+            refreshJob = new WorkbenchJob("Task Progress Update Job") {
+                @Override
+                public IStatus runInUIThread(IProgressMonitor monitor) {
+                    synchronized (refreshInfos) {
+                        for (TaskInfo info : refreshInfos) {
+                            viewer.refresh(info);
+                        }
+                        refreshInfos.clear();
+                    }
+                    return Status.OK_STATUS;
+                }
+
+            };
+            refreshJob.setSystem(true);
+        }
+
+        public void refresh(TaskInfo info) {
+            synchronized (refreshInfos) {
+                refreshInfos.add(info);
+            }
+            if (PlatformUI.isWorkbenchRunning()) {
+                refreshJob.schedule();
+            }
+        }
+    }
+}
