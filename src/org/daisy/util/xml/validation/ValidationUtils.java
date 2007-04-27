74068befc54e458d348c395fb20904370c82366a
Markus Gylling <markus.gylling@gmail.com>
2007-04-27 14:22:51 +0000

    initial commit

---
+++ src/org/daisy/util/xml/validation/ValidationUtils.java
@@ -0,0 +1,81 @@
+package org.daisy.util.xml.validation;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.stream.StreamSource;
+
+import org.daisy.util.location.LocationUtils;
+import org.daisy.util.xml.NamespaceReporter;
+
+/**
+ *
+ * @author Markus Gylling
+ */
+public class ValidationUtils {
+	
+	/**
+	 * Convert an identifier string into one or several schema (RNG, XSD, SCH) <code>javax.xml.transform.Source</code> objects.
+	 * <p>The conversion requires</p>
+	 * <ul>
+	 * <li>that the identifier can be converted into a URL</li>
+	 * <li>that the URL points to an existing resource</li>
+	 * <li>that the resource is an XML document that contains elements belonging to one or several canonical schema namespaces</li>
+	 * </ul>
+	 * <p>If the the resource is a compound schema, a map with several entries will be returned, else one entry.</p> 
+	 * @param identifier a resource identifier consisting of an absolute or relative filespec, or a prolog Public or System Id.
+	 * @return a Map (Source, SchemaLanguageNamespaceURI) representing the input resource. The map is never empty or null.
+	 * @throws TransformerException on any error, and if the resulting map contains no entries (ie no schema namespaces found).
+	 */
+	public static Map<Source,String> toSchemaSources(String identifier) throws TransformerException {				
+		try{
+			URL schemaURL = LocationUtils.identifierToURL(identifier);
+			if(schemaURL == null) throw new TransformerException("Could not convert identifier " + identifier + " into a URL");
+			return ValidationUtils.toSchemaSources(schemaURL);	
+		}catch (Exception e) {
+			throw new TransformerException(e.getMessage(),e);
+		}																
+	}
+
+	/**
+	 * Convert a URL into one or several schema (RNG, XSD, SCH) <code>javax.xml.transform.Source</code> objects.
+	 * <p>The conversion requires</p>
+	 * <ul>
+	 * <li>that the URL points to an existing resource</li>
+	 * <li>that the resource is an XML document that contains elements belonging to one or several canonical schema namespaces</li>
+	 * </ul>
+	 * <p>If the the resource is a compound schema, a map with several entries will be returned, else one entry.</p> 
+	 * @param identifier URL of the single or compound schema.
+	 * @return a Map (Source, SchemaLanguageNamespaceURI) representing the input URL. The map is never empty or null.
+	 * @throws TransformerException on any error, and if the resulting map contains no entries (ie no schema namespaces found).
+	 */
+	public static Map<Source,String> toSchemaSources(URL identifier) throws TransformerException {
+		Map<Source, String> map = new HashMap<Source, String>();				
+		try{										
+			NamespaceReporter nsr = new NamespaceReporter(identifier);	
+			Set<String> nsURIs = nsr.getNamespaceURIs();
+			int nsURIsFound = 0;
+			if(nsURIs!=null){
+				for (String uri : nsURIs) {
+					if(SchemaLanguageConstants.hasEntry(uri)) {
+						++nsURIsFound;
+						StreamSource schss = new StreamSource(identifier.openStream());
+						schss.setSystemId(identifier.toExternalForm());							
+						map.put(schss,uri);	    									
+					}							
+				}
+			}
+			if(nsURIsFound==0) {
+				throw new TransformerException("No schema namespaces found in " + identifier);
+			}
+		}catch (Exception e) {
+			throw new TransformerException(e.getMessage(),e);
+		}		
+		return map;
+	}
+	
+}
