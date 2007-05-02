0ef8e1ed6cad3f21964e4cff429f4c4f27dca428
Markus Gylling <markus.gylling@gmail.com>
2007-05-02 16:05:25 +0000

    Convenience class.

---
+++ src/org/daisy/util/xml/xslt/TransformerFactoryConstants.java
@@ -0,0 +1,12 @@
+package org.daisy.util.xml.xslt;
+
+/**
+ * String constants for our most beloved XSLT TranformerFactories.
+ * @author Markus Gylling
+ */
+public class TransformerFactoryConstants {
+	
+	public static String SAXON8 = "net.sf.saxon.TransformerFactoryImpl";
+	public static String SAXON = "com.icl.saxon.TransformerFactoryImpl";
+		
+}
