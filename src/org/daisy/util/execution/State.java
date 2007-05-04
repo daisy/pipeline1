cbfcd81c16c0d7391165091695c623cc10e8426b
Romain Deltour <rdeltour@gmail.com>
2007-05-04 08:08:20 +0000

    Created the WAITING state

--- src/org/daisy/util/execution/State.java
+++ src/org/daisy/util/execution/State.java
@@ -1,20 +1,20 @@
 /*
- * org.daisy.util - The DAISY java utility library
- * Copyright (C) 2005  Daisy Consortium
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * org.daisy.util - The DAISY java utility library Copyright (C) 2005 Daisy
+ * Consortium
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 package org.daisy.util.execution;
 
@@ -23,4 +23,6 @@ package org.daisy.util.execution;
  * 
  * @author Linus Ericson, Romain Deltour
  */
-public enum State {IDLE, RUNNING, FINISHED, FAILED, ABORTED}
+public enum State {
+    IDLE, WAITING, RUNNING, FINISHED, FAILED, ABORTED
+}
