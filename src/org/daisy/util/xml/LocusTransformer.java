039134befbf9da2ac7355e2057c37b1e4d83d9d5
Markus Gylling <markus.gylling@gmail.com>
2007-05-11 08:17:00 +0000

    support for CSSParseException

--- src/org/daisy/util/xml/LocusTransformer.java
+++ src/org/daisy/util/xml/LocusTransformer.java
@@ -2,7 +2,9 @@ package org.daisy.util.xml;
 
 import javax.xml.stream.Location;
 
+import org.daisy.util.fileset.exception.FilesetFileException;
 import org.daisy.util.fileset.validation.message.ValidatorMessage;
+import org.w3c.css.sac.CSSParseException;
 import org.xml.sax.SAXParseException;
 
 /**
@@ -30,6 +32,40 @@ public class LocusTransformer  {
 			loc.setLineNumber(spe.getLineNumber());
 			loc.setSystemId(spe.getSystemId());
 		}catch (Exception e) {}
+		return loc;
+	}
+	
+	public static Location newLocation(CSSParseException cpe) {		
+		LocationImpl loc =  new LocusTransformer().new LocationImpl();	
+		try{
+			loc.setColumnNumber(cpe.getColumnNumber());
+			loc.setLineNumber(cpe.getLineNumber());
+			loc.setSystemId(cpe.getURI());
+		}catch (Exception e) {}
+		
+		return loc;
+	}
+	
+	public static Location newLocation(FilesetFileException ffe) {
+		LocationImpl loc =  new LocusTransformer().new LocationImpl();	
+		try{
+		    Throwable cause = ffe.getRootCause();
+		    if (cause==null) cause = ffe.getCause();
+		    
+		    Location innerLocation = null;
+			if(cause instanceof CSSParseException) {
+			  innerLocation = LocusTransformer.newLocation((CSSParseException)cause);
+			}else if(cause instanceof SAXParseException) {
+			  innerLocation = LocusTransformer.newLocation((SAXParseException)cause);
+			}
+			
+			loc.setSystemId(ffe.getOrigin().getFile().toURI().toString());
+			if(innerLocation!=null){
+				loc.setLineNumber(innerLocation.getLineNumber());
+				loc.setColumnNumber(innerLocation.getColumnNumber());
+			}
+		}catch (Exception e) {}
+		
 		return loc;
 	}
 	
