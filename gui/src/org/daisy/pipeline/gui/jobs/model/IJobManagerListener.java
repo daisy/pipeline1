e3e14b08d346abc8b92e3e31c7c38175db0ca7ef
romaindeltour <romaindeltour@27897e58-4529-0410-8da3-928601d0d17f>
2007-05-21 16:12:43 +0000

    reverted previous operation, will redo it using 'move' instead of 'copy'

--- gui/src/org/daisy/pipeline/gui/jobs/model/IJobManagerListener.java
+++
@@ -1,8 +0,0 @@
-package org.daisy.pipeline.gui.jobs.model;
-
-import java.util.EventListener;
-
-public interface IJobManagerListener extends EventListener {
-
-    public void jobManagerChanged(JobManagerEvent event);
-}
