d493ee56c1061914da9ef0fccaa978ad54e10f16
romaindeltour <romaindeltour@27897e58-4529-0410-8da3-928601d0d17f>
2007-05-24 10:34:07 +0000

    Fixed bug 1724684: display of location information

--- gui/src/org/daisy/pipeline/gui/messages/MessageField.java
+++ gui/src/org/daisy/pipeline/gui/messages/MessageField.java
@@ -1,5 +1,9 @@
 package org.daisy.pipeline.gui.messages;
 
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.xml.stream.Location;
 
 import org.daisy.dmfc.core.event.MessageEvent;
@@ -50,7 +54,31 @@ class MessageField extends AbstractTableField {
         }
         if (element instanceof Location) {
             Location loc = (Location) element;
-            return loc.toString();
+            StringBuilder locString = new StringBuilder();
+            String sysId = loc.getSystemId();
+            try {
+                URI uri = new URI(sysId);
+                File path = new File(uri.getPath());
+                if (sysId != null && sysId.length() > 0) {
+                    if (loc.getLineNumber() > -1) {
+                        locString.append("At line ");
+                        locString.append(loc.getLineNumber());
+                        if (loc.getColumnNumber() > -1) {
+                            locString.append(", column ");
+                            locString.append(loc.getColumnNumber());
+                        }
+                    }
+                    locString.append(" in file '");
+                    locString.append(path.getName());
+                    locString.append("' [");
+                    locString.append(sysId);
+                    locString.append(']');
+                }
+            } catch (URISyntaxException e) {
+                GuiPlugin.get().error("Couldn't create URI from SystemID", e);
+                locString.append("!err!");
+            }
+            return locString.toString();
         }
         return super.getText(element);
     }
