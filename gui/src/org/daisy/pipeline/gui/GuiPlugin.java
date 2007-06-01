f535fd33701eec7ffc249f3fe696c5bcb156b7c6
romaindeltour <romaindeltour@27897e58-4529-0410-8da3-928601d0d17f>
2007-06-01 13:53:05 +0000

    Propagated changes in the DMFCCore constructor

--- gui/src/org/daisy/pipeline/gui/GuiPlugin.java
+++ gui/src/org/daisy/pipeline/gui/GuiPlugin.java
@@ -1,25 +1,26 @@
 /*
- * DAISY Pipeline GUI
- * Copyright (C) 2006  Daisy Consortium
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * DAISY Pipeline GUI Copyright (C) 2006 Daisy Consortium
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 package org.daisy.pipeline.gui;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.UUID;
 
@@ -150,7 +151,13 @@ public class GuiPlugin extends AbstractUIPlugin {
     }
 
     private void initCore() throws IOException, DMFCConfigurationException {
-        core = new DMFCCore(null, PipelineUtil.getDir(PipelineUtil.HOME_DIR));
+        try {
+            URI propsDir = Platform.getConfigurationLocation().getURL().toURI();
+            core = new DMFCCore(null, PipelineUtil
+                    .getDir(PipelineUtil.HOME_DIR), propsDir);
+        } catch (URISyntaxException e) {
+            error("Unexpected exception", e);//$NON-NLS-1$
+        }
     }
 
     private void initLog() {
