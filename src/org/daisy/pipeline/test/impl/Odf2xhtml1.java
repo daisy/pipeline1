6d0e30908ba29348b221f95b945f48b76571cc2e
Markus Gylling <markus.gylling@gmail.com>
2007-06-04 17:13:35 +0000

    odf to xhtml

---
+++ src/org/daisy/pipeline/test/impl/Odf2xhtml1.java
@@ -0,0 +1,39 @@
+package org.daisy.pipeline.test.impl;
+
+import java.util.List;
+
+import org.daisy.pipeline.test.PipelineTest;
+import org.daisy.util.file.EFolder;
+
+public class Odf2xhtml1 extends PipelineTest {
+
+	public Odf2xhtml1(EFolder dataInputDir, EFolder dataOutputDir) {
+		super(dataInputDir, dataOutputDir);
+	}
+	
+	@Override
+	public List<String> getParameters() {
+		mParameters.add("--odf=" + mDataInputDir + "/odf/simple2.odt");
+		mParameters.add("--output=" + mDataOutputDir + "/Odf2xhtml1/fromOdf.html");				
+		return mParameters;
+	}
+
+	@Override
+	public String getResultDescription() {		
+		return "";
+	}
+
+	@Override
+	public boolean supportsScript(String scriptName) {
+		if("OdfToXhtml.taskScript".equals(scriptName)) {
+			return true;
+		}		
+		return false;
+	}
+
+	@Override
+	public void confirm() {
+		// TODO Auto-generated method stub		
+	}
+
+}
