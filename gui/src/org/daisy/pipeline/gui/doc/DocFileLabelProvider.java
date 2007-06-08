c34f4d6c9e3ef90e96c1226168e9d0b7aaf3d07c
romaindeltour <romaindeltour@27897e58-4529-0410-8da3-928601d0d17f>
2007-06-08 16:50:48 +0000

    Use the XhtmlTitleProvider in the Documentatoin ToCs

--- gui/src/org/daisy/pipeline/gui/doc/DocFileLabelProvider.java
+++ gui/src/org/daisy/pipeline/gui/doc/DocFileLabelProvider.java
@@ -1,25 +1,28 @@
 /*
- * DAISY Pipeline GUI
- * Copyright (C) 2006  Daisy Consortium
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * DAISY Pipeline GUI Copyright (C) 2006 Daisy Consortium
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 package org.daisy.pipeline.gui.doc;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.daisy.pipeline.gui.GuiPlugin;
+import org.daisy.pipeline.gui.util.XhtmlTitleProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
 
@@ -29,20 +32,35 @@ import org.eclipse.swt.graphics.Image;
  */
 public class DocFileLabelProvider extends LabelProvider {
     private TocImageProvider imageProvider;
+    private Map<File, String> titleMap;
 
     public DocFileLabelProvider(TocImageProvider imageProvider) {
         super();
         this.imageProvider = imageProvider;
+        this.titleMap = new HashMap<File, String>();
     }
 
     @Override
     public String getText(Object element) {
         if (element instanceof File) {
             File file = (File) element;
-            if (file.isFile()) {
-                // TODO fetch the html title
+            String title = titleMap.get(file);
+            if (title == null) {
+                if (file.isFile()) {
+                    try {
+                        title = XhtmlTitleProvider.getTitle(file.toURI()
+                                .toURL());
+                    } catch (Exception e) {
+                        GuiPlugin.get().error(
+                                "Couldn't fetch xhtml title of " + file, e);
+                    }
+                }
+                if (title == null || title.length() == 0) {
+                    title = file.getName();
+                }
+                titleMap.put(file, title);
             }
-            return file.getName();
+            return title;
         }
         return null;
     }
