f0ad322fd932742fdaac69c0f091c4661199e550
romaindeltour <romaindeltour@27897e58-4529-0410-8da3-928601d0d17f>
2007-06-08 18:11:51 +0000

    Small bug fixes

--- gui/src/org/daisy/pipeline/gui/progress/JobProgressView.java
+++ gui/src/org/daisy/pipeline/gui/progress/JobProgressView.java
@@ -1,20 +1,19 @@
 /*
- * DAISY Pipeline GUI
- * Copyright (C) 2006  Daisy Consortium
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * DAISY Pipeline GUI Copyright (C) 2006 Daisy Consortium
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 package org.daisy.pipeline.gui.progress;
 
@@ -274,10 +273,12 @@ public class JobProgressView extends ViewPart implements ISelectionListener,
     }
 
     private void refresh() {
-        jobLabel.setText(currJobInfo.getName());
-        stateLabel.setText(Messages.label_state
-                + Timer.format(currJobInfo.getTimer().getElapsedTime()));
-        ((Composite) tasksViewer.getControl()).layout();
+        if (currJobInfo != null) {
+            jobLabel.setText(currJobInfo.getName());
+            stateLabel.setText(Messages.label_state
+                    + Timer.format(currJobInfo.getTimer().getElapsedTime()));
+            ((Composite) tasksViewer.getControl()).layout();
+        }
     }
 
     private void showJob(JobInfo jobInfo) {
