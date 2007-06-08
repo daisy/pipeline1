24aebeeac59eada188abdb44378a6e344e4a5b59
Markus Gylling <markus.gylling@gmail.com>
2007-06-08 11:30:07 +0000

    Update version value with todays date

--- src/org/daisy/util/Version.java
+++ src/org/daisy/util/Version.java
@@ -20,12 +20,12 @@ package org.daisy.util;
 
 /**
  * Exposes global version information on the whole org.daisy.util package.
- * <p>Version information is an ISO8691 date string (YYYY-MM-DD).</p>
+ * <p>Version information is an ISO8601 date string (YYYY-MM-DD).</p>
  * @author Markus Gylling
  */
 public class Version {
 
-	private static final String VERSION = "2006-10-17";
+	private static final String VERSION = "2007-07-08";
 	
 	public static String getVersion() {
 		return VERSION;
