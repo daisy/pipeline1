747a23aeea2a7cae9bc6baea61f375901c769449
Markus Gylling <markus.gylling@gmail.com>
2007-06-20 13:38:59 +0000

    Added correct setters for Xalan

--- src/org/daisy/util/xml/xslt/TransformerFactoryConstants.java
+++ src/org/daisy/util/xml/xslt/TransformerFactoryConstants.java
@@ -8,6 +8,12 @@ public class TransformerFactoryConstants {
 	
 	public static String SAXON8 = "net.sf.saxon.TransformerFactoryImpl";
 	public static String SAXON = "com.icl.saxon.TransformerFactoryImpl";
-	public static String XALAN_XSLTC_INTERNAL = "com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl";
+	public static String XALAN = "org.apache.xalan.processor.TransformerFactoryImpl";
+	public static String XALAN_XSLTC_TRANSLET = "org.apache.xalan.xsltc.trax.TransformerFactoryImpl";
+	
+	/*
+	 * The below should not be used since its a Sun JRE specific identifier
+	 */
+	//public static String XALAN_XSLTC_INTERNAL = "com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl";
 		
 }
