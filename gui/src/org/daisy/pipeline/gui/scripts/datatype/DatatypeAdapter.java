782dc55579d29527560d1d3d9c9bd3079e05d8a7
romaindeltour <romaindeltour@27897e58-4529-0410-8da3-928601d0d17f>
2007-06-22 07:43:02 +0000

    Persist last browses folders in the wizard (following Lynn's request). Input and Output are persisted separately.

--- gui/src/org/daisy/pipeline/gui/scripts/datatype/DatatypeAdapter.java
+++ gui/src/org/daisy/pipeline/gui/scripts/datatype/DatatypeAdapter.java
@@ -1,20 +1,19 @@
 /*
- * DAISY Pipeline GUI
- * Copyright (C) 2006  Daisy Consortium
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * DAISY Pipeline GUI Copyright (C) 2006 Daisy Consortium
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 package org.daisy.pipeline.gui.scripts.datatype;
 
@@ -33,6 +32,9 @@ import org.eclipse.swt.widgets.Widget;
  * 
  */
 public abstract class DatatypeAdapter {
+    public static final String LAST_SELECTED_INPUT = "LAST_SELECTED_INPUT"; //$NON-NLS-1$
+    public static final String LAST_SELECTED_OUTPUT = "LAST_SELECTED_OUTPUT"; //$NON-NLS-1$
+
     private static Map<Datatype.Type, DatatypeAdapter> adapterMap = new HashMap<Datatype.Type, DatatypeAdapter>();
 
     public static DatatypeAdapter getAdapter(Datatype type) {
--- gui/src/org/daisy/pipeline/gui/scripts/datatype/DirectoryAdapter.java
+++ gui/src/org/daisy/pipeline/gui/scripts/datatype/DirectoryAdapter.java
@@ -1,20 +1,19 @@
 /*
- * DAISY Pipeline GUI
- * Copyright (C) 2006  Daisy Consortium
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * DAISY Pipeline GUI Copyright (C) 2006 Daisy Consortium
+ * 
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 package org.daisy.pipeline.gui.scripts.datatype;
 
@@ -22,8 +21,10 @@ import java.io.File;
 
 import org.daisy.pipeline.core.script.ScriptParameter;
 import org.daisy.pipeline.core.script.datatype.DirectoryDatatype;
-import org.daisy.pipeline.gui.util.Messages;
+import org.daisy.pipeline.gui.PreferencesUtil;
 import org.daisy.pipeline.gui.util.DialogHelper;
+import org.daisy.pipeline.gui.util.Messages;
+import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -50,12 +51,16 @@ public class DirectoryAdapter extends DefaultAdapter {
         button.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent e) {
-                File file = new File(field.getText());
+                File file = new File(PreferencesUtil.get(
+                        type.isInput() ? LAST_SELECTED_INPUT
+                                : LAST_SELECTED_OUTPUT, field.getText()));
                 int style = (type.isInput()) ? SWT.OPEN : SWT.SAVE;
                 String path = DialogHelper.browseDir(parent.getShell(), file,
                         style);
                 if (path != null) {
                     field.setText(path);
+                    PreferencesUtil.put(type.isInput() ? LAST_SELECTED_INPUT
+                            : LAST_SELECTED_OUTPUT, path, new InstanceScope());
                 }
             }
         });
