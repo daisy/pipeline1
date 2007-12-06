c8cfd51b6b75980ede2fb63df37202189c7d2526
Markus Gylling <markus.gylling@gmail.com>
2007-12-06 13:03:40 +0000

    Initial commit of a CSS utility package

---
+++ src/org/daisy/util/css/stylesheets/Stylesheets.java
@@ -0,0 +1,63 @@
+/*
+ * org.daisy.util - The DAISY java utility library
+ * Copyright (C) 2005  Daisy Consortium
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package org.daisy.util.css.stylesheets;
+
+import java.net.URL;
+
+/**
+ * A provider of canonical CSS stylesheets
+ * who has their home in this directory. Note; the URL returned
+ * may be a URL to a file in a jar; therefore use url.openStream() 
+ * to get to the data.
+ * @author Markus Gylling
+ */
+
+public class Stylesheets {
+
+	private static final String D202_DEFAULT_CSS = "d202.basic.css";
+	private static final String Z3986_DEFAULT_CSS = "dtbook.2005.basic.css";
+	
+	/**
+	 * @param localName local (no path specified) name of a CSS stylesheet placed in the same folder as this class.
+	 * @return a URL of the stylesheet if found, else null.
+	 */
+	public static URL get(String localName) {
+		return Stylesheets.class.getResource(localName);
+	}
+	
+	/**
+	 * @param docType the document type to provide a stylesheet for
+	 * @return a URL of a default CSS stylesheet for inparam DocumentType.
+	 */
+	public static URL get(DocumentType docType) {
+		if(docType == DocumentType.D202_XHTML) {
+			return Stylesheets.class.getResource(D202_DEFAULT_CSS);
+		}else
+		if(docType == DocumentType.Z3986_DTBOOK) {
+			return Stylesheets.class.getResource(Z3986_DEFAULT_CSS);
+		}
+		throw new IllegalArgumentException();
+	}
+	
+	public static enum DocumentType {
+		Z3986_DTBOOK,
+		D202_XHTML;
+	}
+}
