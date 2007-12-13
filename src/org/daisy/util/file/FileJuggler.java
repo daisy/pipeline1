b831fc71886ddbcb1a40e1b3bda4bfdff46d2fa3
Joel Håkansson <joel.hakansson@mtm.se>
2007-12-13 09:41:10 +0000

    Changes to FileJuggler * Improved handling of internal temp files to avoid a potential file locking problem * Improved exception handling * Updated inline documentation

--- src/org/daisy/util/file/FileJuggler.java
+++ src/org/daisy/util/file/FileJuggler.java
@@ -2,89 +2,108 @@ package org.daisy.util.file;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 
 /**
- * Given an initial input file and the final output file, this class can be 
+ * Given an initial input file and a final output file, this class can be 
  * used to limit the code needed to handle temporary files in a 
  * transformer. After each step (file written) the temporary files are 
  * swapped by calling swap() and you are ready to use the files again. 
  * Very convenient together with optional steps.
  * 
+ * Note: FileJuggler does not work on zero byte files. The output file
+ * must contain data when a call to swap() is made.
+ * 
  * @author  Joel Hakansson, TPB
- * @version 27 sep 2007
+ * @version 13 december 2007
  * @since 1.0
  */
 public class FileJuggler {
-	private TempFile t1;
-	private TempFile t2;
-	private File input;
+	private File t1;
+	private File t2;
 	private File output;
 	private boolean toggle;
 	
 	/**
-	 * Default constructor
+	 * Constructs a new FileJuggler object
+	 * 
 	 * @param input An existing input file
 	 * @param output An output file
-	 * @throws IOException 
+	 * @throws IOException  An IOException is thrown if the input does not exist
+	 * or if the input or output is a directory or if the temporary 
+	 * files could not be created.
 	 */
 	public FileJuggler(File input, File output) throws IOException {
-		this.t1 = null;
-		this.t2 = null;
-		this.toggle = true;
-		this.input = input;
-		this.output = output;
 		if (!input.exists()) throw new FileNotFoundException();
-		this.t1 = new TempFile();
-		this.t2 = new TempFile();
-		//jh20071210, added t2.delete();
-		t2.delete();
-		FileUtils.copy(input, this.t1.getFile());
+		if (!input.isFile() || (output.exists() && !output.isFile())) {
+			throw new IOException("Cannot perform this operation on directories.");
+		}
+		this.toggle = true;
+		this.output = output;
+		this.t1 = TempFile.create();
+		this.t2 = TempFile.create();
+		FileUtils.copy(input, this.t1);
 	}
 
 	/**
 	 * Get the current input file
-	 * @return Returns the current input file
+	 * @return Returns the current input file or null if FileJuggler has been closed
 	 */
 	public File getInput() {
-		return toggle ? t1.getFile() : t2.getFile();
+		return toggle ? t1 : t2;
 	}
 	
 	/**
 	 * Get the current output file
-	 * @return Returns the current output file
+	 * @return Returns the current output file or null if FileJuggler has been closed
 	 */
 	public File getOutput() {
-		return toggle ? t2.getFile() : t1.getFile();
+		return toggle ? t2 : t1;
 	}
 	
 	/**
 	 * Swap the input and output file before writing to the output again
-	 * @throws FileNotFoundException 
+	 * @throws IOException  An IOException is thrown if FileJuggler has been
+	 * closed or if the output file is open or empty.
 	 */
-	public void swap() throws FileNotFoundException {
-		// Check that the soon to be input file exists
-		if (getOutput().exists()) {
+	public void swap() throws IOException {
+		if (t1==null || t2==null) {
+			throw new IllegalStateException("Cannot swap after close.");
+		}
+		if (getOutput().length()>0) {
 			toggle = !toggle;
-			getOutput().delete();
+			// reset the new output to length()=0
+			new FileOutputStream(getOutput()).close();
 		} else {
-			throw new FileNotFoundException("Cannot swap to a non-existing file.");
+			throw new IOException("Cannot swap to an empty file.");
 		}
 	}
 	
 	/**
-	 * Must be called when done
-	 * Closes the temporary files and copies the result to the output file
-	 * @throws IOException 
+	 * Closes the temporary files and copies the result to the output file.
+	 * Closing the FileJuggler is a mandatory last step after which no other
+	 * calls to the object should be made.
+	 * @throws IOException  An IOException is thrown e.g. if FileJuggler is 
+	 * closed or if the temporary files have been deleted.  
 	 */
 	public void close() throws IOException {
-		//mg20071207: added length>0
-		if (getOutput().exists() && getOutput().length()>0) FileUtils.copy(getOutput(), output);
-		else if (getInput().exists() && getInput().length()>0) FileUtils.copy(getInput(), output);
-		else FileUtils.copy(input, output);
-		t1.delete();
-		t2.delete();
+		if (t1==null || t2==null) {
+			throw new IllegalStateException("Already closed.");
+		}
+		try {
+			if (getOutput().length()>0) FileUtils.copy(getOutput(), output);
+			else if (getInput().length()>0) FileUtils.copy(getInput(), output);
+			else {
+				throw new IOException("Temporary files corrupted.");
+			}
+		} finally {
+			t1.delete();
+			t2.delete();
+			t1 = null;
+			t2 = null;
+		}
 	}
 
 }
