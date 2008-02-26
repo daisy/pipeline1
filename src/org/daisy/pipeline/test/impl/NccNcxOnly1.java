a93c0a11eb3578ec87c0ff57f727e13563601de5
Markus Gylling <markus.gylling@gmail.com>
2008-02-26 12:14:15 +0000

    Script and test for se_tpb_nccNcxOnly

---
+++ src/org/daisy/pipeline/test/impl/NccNcxOnly1.java
@@ -0,0 +1,39 @@
+package org.daisy.pipeline.test.impl;
+
+import java.util.List;
+
+import org.daisy.pipeline.test.PipelineTest;
+import org.daisy.util.file.EFolder;
+
+public class NccNcxOnly1 extends PipelineTest {
+
+	public NccNcxOnly1(EFolder dataInputDir, EFolder dataOutputDir) {
+		super(dataInputDir, dataOutputDir);
+	}
+	
+	@Override
+	public List<String> getParameters() {		
+		mParameters.add("--input=" + mDataInputDir + "/dtb/d202/dontworrybehappy/ncc.html");
+		mParameters.add("--output=" + mDataOutputDir + "/NccNcxOnly1/");
+		return mParameters;
+	}
+
+	@Override
+	public String getResultDescription() {		
+		return "";
+	}
+
+	@Override
+	public boolean supportsScript(String scriptName) {
+		if("NccNcxOnly.taskScript".equals(scriptName)) {
+			return true;
+		}		
+		return false;
+	}
+
+	@Override
+	public void confirm() {
+		// TODO Auto-generated method stub		
+	}
+
+}
