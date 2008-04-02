5791a0a416be629067cdf82f4187ec8076865f8a
Joel Håkansson <joel.hakansson@mtm.se>
2008-04-02 14:31:42 +0000

    Initial commit for externalize whitespace. Will change e.g. '<em> example </em>' to ' <em>example</em> '. No gui checkbox yet. Does not run by default.

--- transformers/se_tpb_dtbookFix/DTBookFix.java
+++ transformers/se_tpb_dtbookFix/DTBookFix.java
@@ -291,7 +291,12 @@ public class DTBookFix extends Transformer implements EntityResolver, URIResolve
     		executors.add(new XSLTExecutor(parameters,this.getClass().getResource("./xslt/tidy-remove-empty-elements.xsl"),v2005_1_2,i18n("TIDY_REMOVE_EMPTY_ELEMENTS"),this,this,this,emitter));
     		executors.add(new XSLTExecutor(parameters,this.getClass().getResource("./xslt/tidy-add-author-title.xsl"),v2005_1_2,i18n("ADD_AUTHOR_AND_TITLE"),this,this,this,emitter));
     		executors.add(new LangExecutor(parameters, this.getClass().getResource("./xslt/tidy-add-lang.xsl"), i18n("ADD_LANG"), this, this, this, emitter));
-
+    		
+    		if(((String)parameters.get("externalizeWhitespace")).contentEquals("true")) {
+    			//tidy-externalize-whitespace.xsl is optional
+    			executors.add(new XSLTExecutor(parameters,this.getClass().getResource("./xslt/tidy-externalize-whitespace.xsl"),v2005_1_2,i18n("EXTERNALIZE_WHITESPACE"),this,this,this,emitter));
+    		} 
+    		
     		//run the indenter last in the chain, this is harmless so always active
     		executors.add(new XSLTExecutor(parameters,this.getClass().getResource("./xslt/tidy-indent.xsl"),v2005_1_2,i18n("INDENT"),this,this,this,emitter));
 
