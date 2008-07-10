71e2a099188b540aaa404b02644c4e27af74f3a9
Markus Gylling <markus.gylling@gmail.com>
2008-07-10 11:17:18 +0000

    Added getValue method

--- src/org/daisy/util/xml/stax/AttributeByName.java
+++ src/org/daisy/util/xml/stax/AttributeByName.java
@@ -20,6 +20,7 @@ package org.daisy.util.xml.stax;
 import java.util.Iterator;
 
 import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.StartElement;
 
@@ -37,6 +38,15 @@ public class AttributeByName {
 			a = (Attribute)attributes.next();
 			if(name.equals(a.getName())) return a;			
 		}
+		return null;
+	}
+	
+	public static String getValue(QName name, XMLStreamReader reader) {
+		for (int i = 0; i < reader.getAttributeCount(); i++) {
+			if(reader.getAttributeName(i).equals(name)) {
+				return reader.getAttributeValue(i);
+			}
+		}		
 		return null;
 	}
 }
