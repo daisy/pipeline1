356fadcf14f337ec27e30277957f35037008b737
Romain Deltour <rdeltour@gmail.com>
2009-01-07 08:38:30 +0000

    Added a shell script launcher for the Pipeline RMI app.

---
+++ launchers/pipeline-rmi-dev.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#####################################
+# Parameters:
+#   $1 - The Pipeline instance RMI ID
+#
+# The script is supposed to be called
+# in the Pipeline home directory
+#
+# ###################################
+
+# This is the path to Java. Edit this variable to suit your needs.
+JAVA=`which java 2> /dev/null`
+
+# Add all the libraries to the classpath
+CP=bin/
+for jar in `ls lib/*.jar`; do
+	CP="$jar":$CP
+done
+
+$JAVA -Xms256m -Xmx1024m -cp $CP org.daisy.pipeline.rmi.RMIPipelineApp $1
+
+RESULT=$?
+
+exit $RESULT
