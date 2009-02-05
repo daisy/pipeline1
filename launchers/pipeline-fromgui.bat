c1688cacfab5ff7b30354aaacc985933b6854380
Romain Deltour <rdeltour@gmail.com>
2009-02-05 05:05:39 +0000

    Added launcher scripts for the Pipeline GUI and RMI

---
+++ launchers/pipeline-fromgui.bat
@@ -0,0 +1,14 @@
+@echo off
+
+echo %0
+echo %cd%
+rem This is the path to Java. Edit this variable to suite your needs.
+set JAVA=java
+
+SETLOCAL ENABLEDELAYEDEXPANSION  
+
+set PIPELINE_DIR=..\plugins\org.daisy.pipeline_1.0.3\
+set CP=%PIPELINE_DIR%;%PIPELINE_DIR%\daisy-pipeline.jar;%PIPELINE_DIR%\daisy-util.jar
+for %%f IN (%PIPELINE_DIR%lib\*.jar) do set CP=!CP!;%%f
+
+%JAVA% -classpath %CP% org.daisy.pipeline.ui.CommandLineUI %1 %2 %3 %4 %5 %6 %7 %8 %9
