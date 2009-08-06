b673483989301672ab98a9a29a802f9e2b626e16
Romain Deltour <rdeltour@gmail.com>
2009-08-06 23:15:12 +0000

    Simplified the FilesetType constants by using a Java 5 Enum.

--- src/org/daisy/util/fileset/FilesetType.java
+++ src/org/daisy/util/fileset/FilesetType.java
@@ -18,158 +18,36 @@
 package org.daisy.util.fileset;
 
 /**
- * Gathers constants that represent those types of filesets that this package claims to support
+ * Gathers constants that represent those types of filesets that this package
+ * claims to support
+ * 
  * @author Markus Gylling
+ * @author Romain Deltour
  */
-public final class FilesetType {
-	
-	private FilesetType() {}          
-    public static final FilesetType DAISY_202 = new FilesetType();
-    public static final FilesetType Z3986 = new FilesetType();
-    public static final FilesetType NIMAS = new FilesetType();
-    public static final FilesetType OPS_20 = new FilesetType();
-    public static final FilesetType OPS_EPUB = new FilesetType();
-    public static final FilesetType Z3986_RESOURCEFILE = new FilesetType();
-    public static final FilesetType XHTML_DOCUMENT = new FilesetType();
-    public static final FilesetType HTML_DOCUMENT = new FilesetType();
-    public static final FilesetType DTBOOK_DOCUMENT = new FilesetType();
-    public static final FilesetType CSS = new FilesetType();
-    public static final FilesetType PLAYLIST_M3U = new FilesetType();
-    public static final FilesetType PLAYLIST_PLS = new FilesetType();
-    public static final FilesetType UNKNOWN = new FilesetType();
-    
-    /**
-     * Returns the canonical name constant for this fileset type in string form.
-     * <p>For nicename retrieval, see toNiceNameString.</p>
-     */
-    public String toString(){
-    	if (this.equals(FilesetType.CSS)) {
-    		return "CSS";
-    	}else
-    	if (this.equals(FilesetType.DAISY_202)) {
-    		return "DAISY_202";
-    	}else
-        if (this.equals(FilesetType.DTBOOK_DOCUMENT)) {
-        		return "DTBOOK_DOCUMENT";
-       	}else
-       	if (this.equals(FilesetType.HTML_DOCUMENT)) {
-        		return "HTML_DOCUMENT";
-       	}else
-       	if (this.equals(FilesetType.NIMAS)) {
-        		return "NIMAS";
-       	}else
-           	if (this.equals(FilesetType.OPS_20)) {
-            		return "OPS_20";		
-        }else
-           	if (this.equals(FilesetType.OPS_EPUB)) {
-               		return "OPS_EPUB";            		
-       	}else
-       	if (this.equals(FilesetType.PLAYLIST_M3U)) {
-        		return "PLAYLIST_M3U";
-       	}else
-       	if (this.equals(FilesetType.PLAYLIST_PLS)) {
-        		return "PLAYLIST_PLS";
-       	}else
-       	if (this.equals(FilesetType.XHTML_DOCUMENT)) {
-        		return "XHTML_DOCUMENT";
-       	}else
-       	if (this.equals(FilesetType.Z3986)) {
-        		return "Z3986";
-       	}else 
-       	if (this.equals(FilesetType.Z3986_RESOURCEFILE)) {
-        		return "Z3986_RESOURCEFILE";
-       	}else 
-       	if (this.equals(FilesetType.UNKNOWN)) {
-        		return "UNKNOWN";
-       	}
-	
-    	return null;
-    }
+public enum FilesetType {
 
-    public String toNiceNameString(){
-    	if (this.equals(FilesetType.CSS)) {
-    		return "CSS document";
-    	}else
-    	if (this.equals(FilesetType.DAISY_202)) {
-    		return "DAISY 2.02 DTB";
-    	}else
-        if (this.equals(FilesetType.DTBOOK_DOCUMENT)) {
-        		return "Dtbook document";
-       	}else
-       	if (this.equals(FilesetType.HTML_DOCUMENT)) {
-        		return "HTML document";
-       	}else
-       	if (this.equals(FilesetType.NIMAS)) {
-        		return "NIMAS fileset";
-       	}else
-        if (this.equals(FilesetType.OPS_20)) {
-        	return "OPS 2.0 fileset";        		
-        }else
-            if (this.equals(FilesetType.OPS_EPUB)) {
-            	return "OPS 2.0 EPUB File";        	
-       	}else
-       	if (this.equals(FilesetType.PLAYLIST_M3U)) {
-        		return "M3U playlist";
-       	}else
-       	if (this.equals(FilesetType.PLAYLIST_PLS)) {
-        		return "PLS playlist";
-       	}else
-       	if (this.equals(FilesetType.XHTML_DOCUMENT)) {
-        		return "XHTML document";
-       	}else
-       	if (this.equals(FilesetType.Z3986)) {
-        		return "Z3986 DTB";
-       	}else 
-       	if (this.equals(FilesetType.Z3986_RESOURCEFILE)) {
-        		return "Z3986 Resourcefile";
-       	}else 
-       	if (this.equals(FilesetType.UNKNOWN)) {
-        		return "unknown";
-       	}
-	
-    	return null;
-    }
+	DAISY_202("DAISY 2.02 DTB"),
+	Z3986("Z3986 DTB"),
+	NIMAS("NIMAS fileset"),
+	OPS_20("OPS 2.0 fileset"), 
+	OPS_EPUB("OPS 2.0 EPUB File"),
+	Z3986_RESOURCEFILE("Z3986 Resourcefile"),
+	XHTML_DOCUMENT("XHTML document"),
+	HTML_DOCUMENT("HTML document"),
+	DTBOOK_DOCUMENT("Dtbook document"),
+	CSS("CSS document"),
+	PLAYLIST_M3U("M3U playlist"),
+	PLAYLIST_PLS("PLS playlist"),
+	UNKNOWN("unknown");
 
-    
-    public static FilesetType parse(String type) throws IllegalArgumentException {
-        if ("DAISY_202".equals(type)) {
-            return DAISY_202;
-        }else
-        if ("Z3986".equals(type)) {
-            return Z3986;
-        }else
-        if ("NIMAS".equals(type)) {
-            return NIMAS;
-        }else
-        if ("OPS_20".equals(type)) {
-            return OPS_20;    
-        }else
-        if ("Z3986_RESOURCEFILE".equals(type)) {
-            return Z3986_RESOURCEFILE;
-        }else
-        if ("XHTML_DOCUMENT".equals(type)) {
-            return XHTML_DOCUMENT;
-        }else
-        if ("HTML_DOCUMENT".equals(type)) {
-            return HTML_DOCUMENT;
-        }else
-        if ("DTBOOK_DOCUMENT".equals(type)) {
-            return DTBOOK_DOCUMENT;
-        }else
-        if ("CSS".equals(type)) {
-            return CSS;
-        }else
-        if ("PLAYLIST_M3U".equals(type)) {
-            return PLAYLIST_M3U;
-        }else
-        if ("PLAYLIST_PLS".equals(type)) {
-            return PLAYLIST_PLS;
-        }
-        else
-            if ("UNKNOWN".equals(type)) {
-                return UNKNOWN;
-            }
-        throw new IllegalArgumentException("FileSet type must be one of DAISY_202, Z3986, NIMAS, OPS_20, Z3986_RESOURCEFILE, HTML_DOCUMENT, XHTML_DOCUMENT, DTBOOK_DOCUMENT, CSS, PLAYLIST_M3U, PLAYLIST_PLS, UNKNOWN");
-    }
-	
+	private String nicename;
+
+	private FilesetType(String nicename) {
+		this.nicename = nicename;
+	}
+
+	public String toNiceNameString() {
+		return nicename;
+	}
+
 }
