aba66abcba3cac4dfcd9cad1e2be382f8bbdb2ce
Christian Egli <christian.egli@sbs.ch>
2011-05-11 09:50:08 +0000

    Add the volume split transformer
    This transformer takes a DTBook XML and inserts special div elements
    with a special class attribute, namely "volume-split-point". These can
    then be used by other transformers such as DTBook2LaTeX to split the
    input into volumes.

---
+++ src/org/daisy/pipeline/test/impl/DTBookVolumeSplit.java
@@ -0,0 +1,41 @@
+package org.daisy.pipeline.test.impl;
+
+import java.util.List;
+
+import org.daisy.pipeline.test.PipelineTest;
+import org.daisy.util.file.Directory;
+
+public class DTBookVolumeSplit extends PipelineTest {
+
+	public DTBookVolumeSplit(Directory dataInputDir, Directory dataOutputDir) {
+		super(dataInputDir, dataOutputDir);
+	}
+	
+	@Override
+	public List<String> getParameters() {
+		mParameters.add("--input=" + mDataInputDir + "/dtbook/hauy-2005-1.xml");
+		mParameters.add("--output=" + mDataOutputDir + "/DTBookVolumeSplit/DTBook.xml");
+		mParameters.add("--number_of_volumes="+"3");
+		return mParameters;
+	}
+
+	@Override
+	public String getResultDescription() {		
+		return "";
+	}
+
+	@Override
+	public boolean supportsScript(String scriptName) {
+		if("DTBookVolumeSplit.taskScript".equals(scriptName)) {
+			return true;
+		}		
+		return false;
+	}
+
+	@Override
+	public void confirm() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
