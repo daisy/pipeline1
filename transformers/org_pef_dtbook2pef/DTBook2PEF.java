3ff2e2f0346c6604a2fc9e6b93b976e3de785362
Joel Håkansson <joel.hakansson@mtm.se>
2012-08-20 09:35:03 +0000

    inital commit for dotify 2.0 interface changes (this is not the final build of dotify 2.0)

--- transformers/org_pef_dtbook2pef/DTBook2PEF.java
+++ transformers/org_pef_dtbook2pef/DTBook2PEF.java
@@ -5,10 +5,9 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.daisy.dotify.setups.TaskSystemFactory;
-import org.daisy.dotify.setups.TaskSystemFactory.OutputFormat;
-import org.daisy.dotify.setups.TaskSystemFactory.Setup;
 import org.daisy.dotify.system.InternalTaskException;
+import org.daisy.dotify.system.TaskSystemFactory;
+import org.daisy.dotify.text.FilterLocale;
 import org.daisy.pipeline.core.InputListener;
 import org.daisy.pipeline.core.transformer.Transformer;
 import org.daisy.pipeline.exception.TransformerRunException;
@@ -59,98 +58,20 @@ public class DTBook2PEF extends Transformer {
 		// get parameters
 		File input = new File(parameters.get("input"));
 		File output = new File(parameters.get("output"));
-		File debug = new File(parameters.get("tempFilesDirectory"));
-		Setup setup = Setup.valueOf(parameters.get("setup").replace('-', '_'));
-		OutputFormat outputformat = OutputFormat.valueOf(parameters.get("outputFormat").toUpperCase());
-		//String dateFormat = parameters.get("dateFormat");
-		//boolean writeTempFiles = "true".equals(parameters.get("writeTempFiles"));
-		//String cols = parameters.get("cols");
-		//if (cols==null || "".equals(cols)) {
-//			parameters.remove("cols");
-		//}
+		String setup = parameters.get("setup");
+		FilterLocale locale = FilterLocale.parse(parameters.get("locale"));
 
 		map = new HashMap<String, String>();
 		map.putAll(parameters);
 		org.daisy.dotify.Main dotify = new org.daisy.dotify.Main();
 		try {
-			dotify.run(input, output, outputformat, setup, map);
+			dotify.run(input, output, setup, locale, map);
 		} catch (InternalTaskException e) {
 			throw new TransformerRunException("InternalTaskException", e);
 		} catch (IOException e) {
 			throw new TransformerRunException("IOException", e);
 		}
-		/*
-		map.put("systemRelease", "20100125");
-		map.put("conversionDate", new Date().toString());
-		// Add default values for optional parameters
-		map.put("input-uri", new File(input).toURI().toString());
-		if (map.get("date")==null || "".equals(map.get("date"))) {
-		    Calendar c = Calendar.getInstance();
-		    c.setTime(new Date());
-		    SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
-			map.put("date", sdf.format(c.getTime()));
-		}
-		if (map.get("identifier")==null || "".equals(map.get("identifier"))) {
-			String id = Double.toHexString(Math.random());
-			id = id.substring(id.indexOf('.')+1);
-			id = id.substring(0, id.indexOf('p'));
-			map.put("identifier", "dummy-id-"+ id);
-		}
-		try {
-			// Load additional settings from file
-			if (map.get("config")==null || "".equals(map.get("config"))) {
-				map.remove("config");
-			} else {
-				File config = new File(parameters.get("config"));
-				Properties p = new Properties();
-				FileInputStream in = new FileInputStream(config);
-				p.loadFromXML(in);
-				for (Object key : p.keySet()) {
-					map.put(key.toString(), p.get(key).toString());
-				}
-			}
 
-			// Load setup
-			ArrayList<InternalTask> tasks = null;
-			TaskSystem ts = null;
-			try {
-				ts = new TaskSystemFactory().newTaskSystem(outputformat, setup, map);
-				tasks = ts.compile(map);
-			} catch (TaskSystemException e) {
-				throw new TransformerRunException("Unable to load '" + (ts!=null?ts.getName():"") + "' with parameters " + map.toString(), e);
-			} catch (TaskSystemFactoryException e) {
-				throw new TransformerRunException("Unable to retrieve a TaskSystem", e);
-			}
-			if (tasks==null) {
-				// Should never happen
-				throw new TransformerRunException("Unable to load \"" + (ts!=null?ts.getName():"") + "\"");
-			}
-			sendMessage("About to run TaskSystem \"" + (ts!=null?ts.getName():"") + "\"");
-
-			// Run tasks
-			double i = 0;
-			FileJuggler fj = new FileJuggler(new File(input), output);
-			for (InternalTask task : tasks) {
-				sendMessage("Running " + task.getName());
-				task.execute(fj.getInput(), fj.getOutput());
-				if (writeTempFiles) {
-					String it = ""+((int)i+1);
-					while (it.length()<3) {
-						it = "0" + it; 
-					}
-					FileUtils.copy(fj.getOutput(), new File(debug, "debug_dtbook2pef_" + it + "_" + task.getName().replaceAll("[\\s:]+", "_")));
-				}
-				fj.swap();
-				i++;
-				progress(i/tasks.size());
-			}
-			fj.close();
-			
-		} catch (IOException e) {
-			throw new TransformerRunException("IO error: ", e.getCause());
-		} catch (InternalTaskException e) {
-			throw new TransformerRunException("Error in internal task", e);
-		}*/
 		progress(1);
 		return true;
 	}
