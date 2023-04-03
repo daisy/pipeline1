4214a09f9537f8b81ffb42ad2c6e3e78fd1d28f0
Nicolas Pavie <pavie.nicolas@gmail.com>
2023-04-03 17:18:17 +0200

    Avoid parameters splitting in pipeline.bat
    Batch considers the '=' character as a separator when extracting
    parameters from command line.

--- launchers/pipeline.bat
+++ launchers/pipeline.bat
@@ -4,5 +4,5 @@ rem This is the path to Java. Edit this variable to suite your needs.
 set JAVA=java
 
 echo.
-%JAVA% -classpath "pipeline.jar";"." org.daisy.pipeline.ui.CommandLineUI %1 %2 %3 %4 %5 %6 %7 %8 %9
+%JAVA% -classpath "pipeline.jar";"." org.daisy.pipeline.ui.CommandLineUI %*
 echo.
